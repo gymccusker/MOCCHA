3612ae823bac32db453ba72361ad62f17ed041e2
gyoung410 <g.young1@leeds.ac.uk>
2019-03-18 11:55:28 +0000

    Import functions required

---
+++ iris_cube.py
@@ -0,0 +1,18 @@
+###
+###
+### SCRIPT TO READ IN UM MODEL DATA AS IRIS CUBE
+###
+###
+
+
+import iris
+import numpy as np
+from netCDF4 import Dataset
+import numpy as np
+
+import matplotlib
+import matplotlib.cm as mpl_cm
+import matplotlib.pyplot as plt
+
+import cartopy.crs as crs
+import cartopy.feature as cfe
