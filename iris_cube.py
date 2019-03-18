15d03fbc4e340f943d6f614066b8d8b1a37e2fe5
gyoung410 <g.young1@leeds.ac.uk>
2019-03-18 14:52:30 +0000

    Add basic read cube commands

--- iris_cube.py
+++ iris_cube.py
@@ -16,3 +16,11 @@ import matplotlib.pyplot as plt
 
 import cartopy.crs as crs
 import cartopy.feature as cfe
+
+root_dir = '~/cylc-run/u-bg610/share/cycle/20160401T0000Z/HighArctic/1p5km/RA2M_CON/um/'
+
+filename1 = root_dir + 'umnsaa_pvera000'
+
+cube1 = iris.load(filename1)
+
+print cube1 # lists all diagnostics in file
