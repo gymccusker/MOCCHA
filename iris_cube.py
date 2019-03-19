57aed88f5cdc7f477bd5ad664fe5d9ea474ebb59
gyoung410 <g.young1@leeds.ac.uk>
2019-03-19 12:37:17 +0000

    add filenames from initial UM trial run (GLM_UM and UM diagnostic files)

--- iris_cube.py
+++ iris_cube.py
@@ -19,8 +19,21 @@ import cartopy.feature as cfe
 
 root_dir = '~/cylc-run/u-bg610/share/cycle/20160401T0000Z/HighArctic/1p5km/RA2M_CON/um/'
 
-filename1 = root_dir + 'umnsaa_pvera000'
+#### 	FILE NAMES
+#In [15]: ls
+#umnsaa_cb000  umnsaa_cb009  umnsaa_cb018  umnsaa_cb027     umnsaa_pverb000  umnsaa_pverc024
+#umnsaa_cb001  umnsaa_cb010  umnsaa_cb019  umnsaa_cb028     umnsaa_pverb006  umnsaa_pverd000
+#umnsaa_cb002  umnsaa_cb011  umnsaa_cb020  umnsaa_pa000     umnsaa_pverb012  umnsaa_pverd006
+#umnsaa_cb003  umnsaa_cb012  umnsaa_cb021  umnsaa_pb000     umnsaa_pverb018  umnsaa_pverd012
+#umnsaa_cb004  umnsaa_cb013  umnsaa_cb022  umnsaa_pvera000  umnsaa_pverb024  umnsaa_pverd018
+#umnsaa_cb005  umnsaa_cb014  umnsaa_cb023  umnsaa_pvera006  umnsaa_pverc000  umnsaa_pverd024
+#umnsaa_cb006  umnsaa_cb015  umnsaa_cb024  umnsaa_pvera012  umnsaa_pverc006  umnsa.stash
+#umnsaa_cb007  umnsaa_cb016  umnsaa_cb025  umnsaa_pvera018  umnsaa_pverc012  umnsa.xhist
+#umnsaa_cb008  umnsaa_cb017  umnsaa_cb026  umnsaa_pvera024  umnsaa_pverc018
+
+filename1 = root_dir + 'umnsaa_pverd000'
 
 cube1 = iris.load(filename1)
 
 print cube1 # lists all diagnostics in file
+
