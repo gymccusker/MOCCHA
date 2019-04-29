fix string of cube.units in writeNetCDF

UM/diags_CloudNet.py
@@ -452,9 +452,9 @@ def writeNetCDF(cube, outfile):
     ###################################
     ###################################
     data = dataset.createVariable(cube.standard_name, np.float32, ('time','grid_latitude','grid_longitude',),fill_value='-9999')
-    data.units = cube.units
+    data.units = str(cube.units)
     data.comment = cube.metadata
-    data[:,:,:] = cube.data[:]
+    data[:] = cube.data[:]
 
     ###################################
     ###################################