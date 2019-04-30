742bcc1cd776e050a7a97fcfdc99885525d57c5f
gyoung410 <G.Young1@leeds.ac.uk>
2019-04-30 09:56:02 +0100

    change projection to NP stereo

--- UM/diags_CloudNet.py
+++ UM/diags_CloudNet.py
@@ -268,7 +268,7 @@ def plot_cartmap(ship_data, cube):
     import cartopy.crs as ccrs
 
     plt.figure()
-    plt.axes(projection=ccrs.PlateCarree())
+    plt.axes(projection=ccrs.NorthPolarStereo())
     iplt.pcolormesh(cube[0,:,:])
     plt.gca().stock_img()
     plt.gca().coastlines()
