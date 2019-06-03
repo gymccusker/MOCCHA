8e10789aa6f7cfd3d3f493398fa4e7647f91d65c
gyoung410 <G.Young1@leeds.ac.uk>
2019-06-03 11:57:49 +0100

    comments

--- UM/use_NetCDF.py
+++ UM/use_NetCDF.py
@@ -214,8 +214,8 @@ def plot_cartmap(ship_data, cube, hour): #, lon, lat):
     ax = plt.axes(projection=ccrs.NorthPolarStereo(central_longitude=30))
 
     ### set size
-    ax.set_extent([0, 60, 87.75, 90], crs=ccrs.PlateCarree())
-    # ax.set_extent([-180, 190, 80, 90], crs=ccrs.PlateCarree())
+    ax.set_extent([0, 60, 87.75, 90], crs=ccrs.PlateCarree())       ### SWATH
+    # ax.set_extent([-180, 190, 80, 90], crs=ccrs.PlateCarree())    ### WHOLE
 
     ### DON'T USE PLATECARREE, NORTHPOLARSTEREO (on it's own), LAMBERT
 
