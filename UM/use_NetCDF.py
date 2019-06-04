e235e9066ed561cc044ed5b7fef874c156b4b8fd
gyoung410 <g.young1@leeds.ac.uk>
2019-06-04 10:12:46 +0100

    change for JASMIN, save fig

--- UM/use_NetCDF.py
+++ UM/use_NetCDF.py
@@ -552,7 +552,7 @@ def plot_cartmap(ship_data, cube, hour): #, lon, lat):
     print 'Finished plotting cartopy map! :)'
     print ''
 
-    # plt.savefig('FIGS/12-13Aug_Outline_wShipTrackMAPPED.svg')
+    plt.savefig('FIGS/12-13Aug_Outline_wShipTrackMAPPED.svg')
     plt.show()
 
 def unrotateGrid(cube):
@@ -614,7 +614,7 @@ def main():
     print ''
 
     ### CHOOSE PLATFORM (OPTIONS BELOW)
-    platform = 'LAPTOP'
+    platform = 'JASMIN'
 
     ### JASMIN
     ### LAPTOP
