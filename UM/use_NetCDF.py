18a9192e064f26bc0ec78a24f796a9203aaa24e3
gyoung410 <g.young1@leeds.ac.uk>
2019-06-05 15:48:22 +0100

    plot 13th to check

--- UM/use_NetCDF.py
+++ UM/use_NetCDF.py
@@ -253,8 +253,8 @@ def gridShipTrack(cube, xoffset, yoffset):
     lat0 = np.array([477,477,477,476])
     tim0 = np.zeros([np.size(lon0)])
     tim0[:] = 0.0
-    # for i in range(0,np.size(lon0)):
-    #     iplt.scatter(cube.dim_coords[2][lon0[i] + xoffset], cube.dim_coords[1][lat0[i] + yoffset],color='yellow')
+    for i in range(0,np.size(lon0)):
+        iplt.scatter(cube.dim_coords[2][lon0[i] + xoffset], cube.dim_coords[1][lat0[i] + yoffset],color='yellow')
 
     ### box pick 1-2h    (13th aug)
     lon1 = np.array([252,253])
@@ -264,8 +264,8 @@ def gridShipTrack(cube, xoffset, yoffset):
     tim_138 = np.append(tim0, tim1)
     lat_138 = np.append(lat0, lat1)
     lon_138 = np.append(lon0, lon1)
-    # for i in range(0,np.size(lon1)):
-    #     iplt.scatter(cube.dim_coords[2][lon1[i] + xoffset], cube.dim_coords[1][lat1[i] + yoffset],color='black')
+    for i in range(0,np.size(lon1)):
+        iplt.scatter(cube.dim_coords[2][lon1[i] + xoffset], cube.dim_coords[1][lat1[i] + yoffset],color='black')
 
     ### box pick 2-3h    (13th aug)
     lon2 = np.array([253,253,254])
@@ -275,8 +275,8 @@ def gridShipTrack(cube, xoffset, yoffset):
     tim_138 = np.append(tim_138, tim2)
     lat_138 = np.append(lat_138, lat2)
     lon_138 = np.append(lon_138, lon2)
-    # for i in range(0,np.size(lon2)):
-    #     iplt.scatter(cube.dim_coords[2][lon2[i] + xoffset], cube.dim_coords[1][lat2[i] + yoffset],color='red')
+    for i in range(0,np.size(lon2)):
+        iplt.scatter(cube.dim_coords[2][lon2[i] + xoffset], cube.dim_coords[1][lat2[i] + yoffset],color='red')
 
     ### box pick 3-4h    (13th aug)
     lon3 = np.array([254,254,255,255])
@@ -286,8 +286,8 @@ def gridShipTrack(cube, xoffset, yoffset):
     tim_138 = np.append(tim_138, tim3)
     lat_138 = np.append(lat_138, lat3)
     lon_138 = np.append(lon_138, lon3)
-    # for i in range(0,np.size(lon3)):
-    #     iplt.scatter(cube.dim_coords[2][lon3[i] + xoffset], cube.dim_coords[1][lat3[i] + yoffset],color='blue')
+    for i in range(0,np.size(lon3)):
+        iplt.scatter(cube.dim_coords[2][lon3[i] + xoffset], cube.dim_coords[1][lat3[i] + yoffset],color='blue')
 
     ### box pick 4-5h    (13th aug)
     lon4 = np.array([255,256])
@@ -297,8 +297,8 @@ def gridShipTrack(cube, xoffset, yoffset):
     tim_138 = np.append(tim_138, tim4)
     lat_138 = np.append(lat_138, lat4)
     lon_138 = np.append(lon_138, lon4)
-    # for i in range(0,np.size(lon4)):
-    #     iplt.scatter(cube.dim_coords[2][lon4[i] + xoffset], cube.dim_coords[1][lat4[i] + yoffset],color='green')
+    for i in range(0,np.size(lon4)):
+        iplt.scatter(cube.dim_coords[2][lon4[i] + xoffset], cube.dim_coords[1][lat4[i] + yoffset],color='green')
 
     ### box pick 5-6h
     lon5 = np.array([256,256,255,255])
@@ -357,12 +357,6 @@ def gridShipTrack(cube, xoffset, yoffset):
     for i in range(0,np.size(lon23)):
         iplt.scatter(cube.dim_coords[2][lon23[i] + xoffset], cube.dim_coords[1][lat23[i] + yoffset],color='magenta')
 
-    # print ''
-    # print 'tim_138 = ' + str(tim_138.shape)
-    # print 'lat_138 = ' + str(lat_138.shape)
-    # print 'lon_138 = ' + str(lon_138.shape)
-    # print ''
-
     # out = writeoutGrid(tim_138, lat_138, lon_138, date)
 
 
@@ -371,8 +365,8 @@ def trackShip(data):
     ###################################
     ## DEFINE METUM PERIOD (CLOUDNET COMPARISON)
     ###################################
-    trackShip_start = np.where(np.logical_and(np.logical_and(data.values[:,2]==12,data.values[:,1]==8),data.values[:,3]>=0))
-    trackShip_end = np.where(np.logical_and(np.logical_and(data.values[:,2]==13,data.values[:,1]==8),data.values[:,3]==1))
+    trackShip_start = np.where(np.logical_and(np.logical_and(data.values[:,2]==13,data.values[:,1]==8),data.values[:,3]>=0))
+    trackShip_end = np.where(np.logical_and(np.logical_and(data.values[:,2]==14,data.values[:,1]==8),data.values[:,3]==1))
     trackShip_index = range(trackShip_start[0][0],trackShip_end[0][-1])
 
     print '******'
