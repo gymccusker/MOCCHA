18df9d3e790daf3f2c9b1d687b0cd77b92ec8dfe
gyoung410 <g.young1@leeds.ac.uk>
2019-06-05 16:46:33 +0100

    17-18h

--- UM/use_NetCDF.py
+++ UM/use_NetCDF.py
@@ -437,8 +437,8 @@ def trackShip(data):
     ###################################
     ## DEFINE METUM PERIOD (CLOUDNET COMPARISON)
     ###################################
-    trackShip_start = np.where(np.logical_and(np.logical_and(data.values[:,2]==14,data.values[:,1]==8),data.values[:,3]>=16))
-    trackShip_end = np.where(np.logical_and(np.logical_and(data.values[:,2]==14,data.values[:,1]==8),data.values[:,3]==18))
+    trackShip_start = np.where(np.logical_and(np.logical_and(data.values[:,2]==14,data.values[:,1]==8),data.values[:,3]>=17))
+    trackShip_end = np.where(np.logical_and(np.logical_and(data.values[:,2]==14,data.values[:,1]==8),data.values[:,3]==19))
     trackShip_index = range(trackShip_start[0][0],trackShip_end[0][-1])
 
     print '******'
