f6a79d4e244c8891c37c6f4220df37b92380bb3f
gyoung410 <g.young1@leeds.ac.uk>
2019-06-05 16:26:30 +0100

    just plot full grid outline

--- UM/use_NetCDF.py
+++ UM/use_NetCDF.py
@@ -580,9 +580,9 @@ def plot_cartmap(ship_data, cube, hour): #, lon, lat):
     #################################################################
     ### draw outline of grid
     # qplt.outline(cube[hour,380:500,230:285])          ### original swath
-    qplt.outline(cube[hour,386:479,211:305])          ### redesigned swath (>13th)
+    # qplt.outline(cube[hour,386:479,211:305])          ### redesigned swath (>13th)
     # qplt.outline(cube[hour,471:495,240:264])          ### 12-13th Aug swath
-    # qplt.outline(cube[hour,:,:])
+    qplt.outline(cube[hour,:,:])
 
     gridship = gridShipTrack(cube, xoffset, yoffset)
 
