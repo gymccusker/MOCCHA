d97d5eeac21f817338c239eb0d5ebc9cf08d4492
gyoung410 <G.Young1@leeds.ac.uk>
2019-06-13 17:45:18 +0100

    should be 95 not 94

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -5545,7 +5545,7 @@ def plot_cartmap(ship_data, cube, hour, grid_filename): #, lon, lat):
     # elif cube[0,0].shape >= 93-1:    # ll = 211, 386
         xoffset = -210
         yoffset = -385
-    elif len(cube[0].dim_coords[-1].points) == 94:
+    elif len(cube[0].dim_coords[-1].points) == 95:
     # elif cube[0,0].shape >= 93-1:    # ll = 211, 386
         xoffset = -210
         yoffset = -385
@@ -5779,10 +5779,10 @@ def pullTrack(cube, grid_filename, con):
     # elif cube[0,0].shape >= 93-1:    # ll = 211, 386
         xoffset = -210
         yoffset = -385
-    elif len(cube[0].dim_coords[-1].points) == 94:
+    elif len(cube[0].dim_coords[-1].points) == 95:
     # elif cube[0,0].shape >= 93-1:    # ll = 211, 386
         xoffset = -210
-        yoffset = -385        
+        yoffset = -385
     else:
     # elif cube[0,0].shape >= 500-1:
         xoffset = 0
