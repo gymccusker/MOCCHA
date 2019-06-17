2f13973e94d31685fd262cc91bc040fb376228df
gyoung410 <G.Young1@leeds.ac.uk>
2019-06-17 14:17:42 +0100

    add wind comments to appended metadata

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -6190,13 +6190,14 @@ def appendNetCDF(outfile):
     ## Global Attributes
     ###################################
     dataset.title = 'Met Office Unified Model single-site (Oden) output during MOCCHA'
-    micro = 'Cloud microphysics: Smith (1990) but includes a cloud/precipitation microphysical scheme with prognostic ice (Wilson and Ballard, 1999), based on Rutledge and Hobbs (1983)'
+    micro = 'Cloud microphysics: Smith (1990) but includes a cloud/precipitation microphysical scheme with prognostic ice (Wilson and Ballard, 1999), based on Rutledge and Hobbs (1983). '
+    wind = 'U wind component evaluated at the mid-points of the left/right grid faces, V components at the mid-points of the upper/lower grid faces. Use height_0 for when plotting these variables. '
     dataset.description = 'Hourly data taken from grid box closest to ship location. Where the ship covers more than one grid box within an hour period, data are averaged from all grid boxes crossed.'
     dataset.history = 'Created ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' by Gillian Young (G.Young1@leeds.ac.uk) using Python (Iris).'
     # dataset.source = 'UK Met Office Unified Model, version 11.1. Microphysics = ' + micro
     # dataset.references = 'N/A'
     dataset.project = 'MOCCHA: Microbiology-Ocean-Cloud Coupling in the High Arctic.'
-    dataset.comment = micro
+    dataset.comment = micro + wind
     dataset.institution = 'University of Leeds.'
     # dataset.initialization_time = outfile[0:4] + '-' + outfile[4:6] + '-' + outfile[6:8]) + ' 00:00:00 UTC.'
     dataset.initialization_time = outfile[0:4] + '-' + outfile[4:6] + '-' + str(int(outfile[6:8]) - 1) + ' 12:00:00 UTC.'
