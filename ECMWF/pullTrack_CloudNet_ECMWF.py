df5822a607acae95be63e9f0106a8b34a26ed420
gyoung410 <G.Young1@leeds.ac.uk>
2019-06-18 15:07:05 +0100

    start at end of drift, might be easier

--- ECMWF/pullTrack_CloudNet_ECMWF.py
+++ ECMWF/pullTrack_CloudNet_ECMWF.py
@@ -686,7 +686,7 @@ def main():
     ### -------------------------------------------------------------------------
     ### define input filenames
     ### -------------------------------------------------------------------------
-    date = '20180812'
+    date = '20180914'
     base_name = date + '_moccha_ecmwf_'
     names = [None] * 38         ## 'empty' list of 38 elements. can assign index without list.append
     filenames = [None] * 38
