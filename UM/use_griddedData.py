599d98933a5d18161fe21fa5cef1c173fde748c9
gyoung410 <G.Young1@leeds.ac.uk>
2019-07-16 18:19:16 +0100

    up to 20180827 included

--- UM/use_griddedData.py
+++ UM/use_griddedData.py
@@ -769,15 +769,16 @@ def main():
     tempnames = ['umnsaa_pa012_r0.nc','umnsaa_pb012_r0.nc','umnsaa_pc011_r0.nc','umnsaa_pd011_r0.nc','20180812_oden_metum.nc']
     names = ['20180812_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
             '20180818_oden_metum.nc','20180819_oden_metum.nc','20180821_oden_metum.nc',
-            '20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc']
+            '20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
+            '20180826_oden_metum.nc','20180827_oden_metum.nc']
 
     missing_files = ['20180813_oden_metum.nc','20180814_oden_metum.nc','20180817_oden_metum.nc',
-            '20180820_oden_metum.nc','20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc',
-            '20180828_oden_metum.nc','20180829_oden_metum.nc','20180830_oden_metum.nc','20180831_oden_metum.nc']
+            '20180820_oden_metum.nc','20180825_oden_metum.nc','20180828_oden_metum.nc',
+            '20180829_oden_metum.nc','20180830_oden_metum.nc','20180831_oden_metum.nc']
     # names = ['umnsaa_pa000.nc','umnsaa_pc000.nc']       ### DEFAULT OUTPUT NAMES FOR TESTING
 
     ## Flag for individual file or monthly:
-    combine = 1
+    combine = 0
 
     if combine == 0:
         filename1 = root_dir + out_dir + names[-1]
