c62df9e6005f0b26c4f5bff8d909e3a40170952f
gyoung410 <G.Young1@leeds.ac.uk>
2019-07-16 15:21:29 +0100

    forgot to uncomment pullTrack_CloudNet function call

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -6511,9 +6511,9 @@ def main():
                     # -------------------------------------------------------------
                     # Pull gridded ship track from cube
                     # -------------------------------------------------------------
-                    #### LOAD CUBE
-                    # if con_flag == 0: fcube, outfile = pullTrack_CloudNet(cube, grid_filename, var_con, stream, date)
-                    # if con_flag == 1: fcube, outfile = pullTrack_CloudNet(cube, grid_filename, global_con, stream, date)
+                    ### LOAD CUBE
+                    if con_flag == 0: fcube, outfile = pullTrack_CloudNet(cube, grid_filename, var_con, stream, date)
+                    if con_flag == 1: fcube, outfile = pullTrack_CloudNet(cube, grid_filename, global_con, stream, date)
 
                 # if stream == '_pb012':
                 #     ### -------------------------------------------------------------------------
