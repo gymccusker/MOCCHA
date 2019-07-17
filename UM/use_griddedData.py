6638a2faa2115a138cbccd59610bb5a7461e8376
gyoung410 <G.Young1@leeds.ac.uk>
2019-07-17 11:31:36 +0100

    sorted 20180817, 20180828,and 20180829

--- UM/use_griddedData.py
+++ UM/use_griddedData.py
@@ -767,17 +767,17 @@ def main():
     ### define input filename
     ### -------------------------------------------------------------------------
     tempnames = ['umnsaa_pa012_r0.nc','umnsaa_pb012_r0.nc','umnsaa_pc011_r0.nc','umnsaa_pd011_r0.nc','20180812_oden_metum.nc']
-    Aug_names = ['20180815_oden_metum.nc','20180816_oden_metum.nc',
+    Aug_names = ['20180815_oden_metum.nc','20180816_oden_metum.nc','20180817_oden_metum.nc',
             '20180818_oden_metum.nc','20180819_oden_metum.nc','20180821_oden_metum.nc',
             '20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
-            '20180826_oden_metum.nc','20180827_oden_metum.nc','20180830_oden_metum.nc',
-            '20180831_oden_metum.nc']
+            '20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
+            '20180829_oden_metum.nc','20180830_oden_metum.nc','20180831_oden_metum.nc']
 
     Sep_names = ['20180901_oden_metum.nc','20180902_oden_metum.nc','20180903_oden_metum.nc','20180904_oden_metum.nc',
-            '20180905_oden_metum.nc','20180906_oden_metum.nc']
+            '20180905_oden_metum.nc','20180906_oden_metum.nc','20180907_oden_metum.nc']
 
-    Aug_missing_files = ['20180812_oden_metum.nc','20180813_oden_metum.nc','20180814_oden_metum.nc','20180817_oden_metum.nc',
-            '20180820_oden_metum.nc','20180825_oden_metum.nc','20180828_oden_metum.nc','20180829_oden_metum.nc']
+    Aug_missing_files = ['20180812_oden_metum.nc','20180813_oden_metum.nc','20180814_oden_metum.nc',
+            '20180820_oden_metum.nc','20180825_oden_metum.nc']
 
     Sep_missing_files = ['20180908_oden_metum.nc','20180909_oden_metum.nc','20180910_oden_metum.nc',
             '20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc','20180914_oden_metum.nc']
@@ -786,14 +786,14 @@ def main():
     # names = ['umnsaa_pa000.nc','umnsaa_pc000.nc']       ### DEFAULT OUTPUT NAMES FOR TESTING
 
     ## Flag for individual file or monthly:
-    combine = 1
+    combine = 0
     ## Choose month:
-    names = Sep_names
-    missing_files = Sep_missing_files
-    month_flag = 9
+    names = Aug_names
+    missing_files = Aug_missing_files
+    month_flag = 8
 
     if combine == 0:
-        filename1 = root_dir + out_dir + names[-1]
+        filename1 = root_dir + out_dir + names[-3]
         print filename1
         print ''
 
