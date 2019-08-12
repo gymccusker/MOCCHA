abb4e39eed307288db6d02ff1456f92386b14a03
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-12 10:28:56 +0100

    change data size definition for IWP and LWP

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -5891,7 +5891,13 @@ def pullTrack_CloudNet(cube, grid_filename, con, stream, date):
                 print 'Variable is 3D:'
                 print ''
                 #### create empty arrays to be filled
-                if stream == '_pb009': data = np.zeros([len(cubetime)-1])
+                if stream == '_pb009':
+                    if cube[k].long_name = 'large_scale_ice_water_path':
+                        data = np.zeros([len(cubetime)])
+                    elif cube[k].long_name = 'large_scale_liquid_water_path':
+                        data = np.zeros([len(cubetime)])
+                    else:
+                        data = np.zeros([len(cubetime)-1])
                 if stream == '_pa012': data = np.zeros([len(cubetime)])
                 dim_flag = 0       ### for next loops
                 print 'data.shape = ', str(data.shape)
