caa9e31a346b2af29e8d70549d811573e360eb16
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-12 11:14:20 +0100

    add suite names to directories for record keeping

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -6617,14 +6617,14 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '4_RA2M_CON/'
+    out_dir = '4_u-bg610_RA2M_CON/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 1_20160401_61DIAG_TEST/
     ## 2_20180801_61DIAGS_TEST/2_30_86.625/
     ## 3_12AUG_SWATH_2FCSTS/
     ## 3_1AUG_SWATH_2FCSTS/
-    ## 4_RA2M_CON
+    ## 4_u-bg610_RA2M_CON/
 
     # -------------------------------------------------------------
     # Load ship track
