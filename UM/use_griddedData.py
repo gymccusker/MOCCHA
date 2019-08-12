9ed69ac3d1bc830bdcebbb67c41a7058100f95b1
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-12 12:25:48 +0100

    move 20180825 to included files

--- UM/use_griddedData.py
+++ UM/use_griddedData.py
@@ -1216,7 +1216,7 @@ def main():
     tempnames = ['umnsaa_pa012_r0.nc','umnsaa_pb012_r0.nc','umnsaa_pc011_r0.nc','umnsaa_pd011_r0.nc','20180812_oden_metum.nc']
     Aug_names = ['20180813_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc','20180817_oden_metum.nc',
             '20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc','20180821_oden_metum.nc',
-            '20180822_oden_metum.nc','20180823_oden_metum.nc','20180826_oden_metum.nc',
+            '20180822_oden_metum.nc','20180823_oden_metum.nc','20180825_oden_metum.nc','20180826_oden_metum.nc',
             '20180827_oden_metum.nc','20180828_oden_metum.nc','20180829_oden_metum.nc',
             '20180830_oden_metum.nc','20180831_oden_metum.nc']
 
@@ -1226,7 +1226,7 @@ def main():
             '20180913_oden_metum.nc','20180914_oden_metum.nc']
 
     Aug_missing_files = ['20180812_oden_metum.nc','20180814_oden_metum.nc',
-            '20180824_oden_metum.nc','20180825_oden_metum.nc']
+            '20180824_oden_metum.nc']
 
     Sep_missing_files = ['20180908_oden_metum.nc']
 
