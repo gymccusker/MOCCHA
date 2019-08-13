ed6ed444d4815c383c072349beb75735f24267f7
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-13 09:41:03 +0100

    change to r1

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -6572,7 +6572,7 @@ def appendMetaNetCDF(outfile, date, out_dir):
     ## Global Attributes
     ###################################
     dataset.title = 'Met Office Unified Model single-site (Oden) output during MOCCHA'
-    revision = 'Revision no. 0. '
+    revision = 'Revision no. 1. '
     if out_dir[2:9] == 'u-bg610':
         micro = 'Cloud microphysics: Smith (1990) but includes a cloud/precipitation microphysical scheme with prognostic ice (Wilson and Ballard, 1999), based on Rutledge and Hobbs (1983). '
     elif out_dir[2:9] == 'u-bl616':
