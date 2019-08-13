3ded35c82b69f32c541d071fc1f5b4b7ff1901af
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-13 09:10:42 +0100

    casim appending works! re-do ra2m output now with working excludeZeros

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -6762,7 +6762,7 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '5_u-bl616_RA2M_CASIM/'
+    out_dir = '4_u-bg610_RA2M_CON/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 1_20160401_61DIAG_TEST/
