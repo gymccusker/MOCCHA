d36160fa000d80ff2d95b622d053538ff2dc2504
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-15 11:54:20 +0100

    change test date back to 20180901

--- ECMWF/pullTrack_CloudNet_ECMWF.py
+++ ECMWF/pullTrack_CloudNet_ECMWF.py
@@ -990,7 +990,7 @@ def main():
     ### -------------------------------------------------------------------------
     ### define input filenames
     ### -------------------------------------------------------------------------
-    date = '20180903'
+    date = '20180901'
     base_name = date + '_moccha_ecmwf_'
     names = [None] * 38         ## 'empty' list of 38 elements. can assign index without list.append
     filenames = [None] * 38
