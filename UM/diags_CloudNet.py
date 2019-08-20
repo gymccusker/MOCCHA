cd39cf091754cb55c348f71d1a7391e6a4859396
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-20 09:14:40 +0100

    change expt name to match filenames

--- UM/diags_CloudNet.py
+++ UM/diags_CloudNet.py
@@ -785,7 +785,7 @@ def main():
         ship_filename = '/nfs/a96/MOCCHA/working/gillian/ship/2018_shipposition_1hour.txt'
 
     ### CHOSEN RUN
-    out_dir = '6_u-bl961_FNdrop_RA2M_CASIM/'
+    out_dir = '6_u-bl961_RA2M_CASIM/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 1_20160401_61DIAG_TEST/
