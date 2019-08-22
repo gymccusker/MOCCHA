2609f262694335ef4e41e8f562448c21f21a0510
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-22 19:39:48 +0100

    plotting all August files completed up til now

--- ECMWF/use_griddedData.py
+++ ECMWF/use_griddedData.py
@@ -1518,10 +1518,10 @@ def main():
     ### define input filename
     ### -------------------------------------------------------------------------
     # tempnames = ['umnsaa_pa012_r0.nc','umnsaa_pb012_r0.nc','umnsaa_pc011_r0.nc','umnsaa_pd011_r0.nc','20180812_oden_ecmwf.nc']
-    Aug_names = ['20180814_oden_ecmwf.nc','20180816_oden_ecmwf.nc','20180818_oden_ecmwf.nc']#,
-            # '20180819_oden_ecmwf.nc','20180820_oden_ecmwf.nc',
-            # '20180821_oden_ecmwf.nc','20180822_oden_ecmwf.nc','20180823_oden_ecmwf.nc','20180824_oden_ecmwf.nc',
-            # '20180825_oden_ecmwf.nc','20180826_oden_ecmwf.nc','20180827_oden_ecmwf.nc','20180828_oden_ecmwf.nc',
+    Aug_names = ['20180814_oden_ecmwf.nc','20180816_oden_ecmwf.nc','20180818_oden_ecmwf.nc',
+            '20180819_oden_ecmwf.nc','20180820_oden_ecmwf.nc','20180821_oden_ecmwf.nc',
+            '20180822_oden_ecmwf.nc','20180823_oden_ecmwf.nc','20180824_oden_ecmwf.nc',
+            '20180825_oden_ecmwf.nc']#,'20180826_oden_ecmwf.nc','20180827_oden_ecmwf.nc','20180828_oden_ecmwf.nc',
             # '20180829_oden_ecmwf.nc','20180830_oden_ecmwf.nc','20180831_oden_ecmwf.nc']
 
     Sep_names = ['20180901_oden_ecmwf.nc','20180902_oden_ecmwf.nc','20180903_oden_ecmwf.nc',
