503aabe48c12aa7569429b4af891acb9284ecd1d
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-22 11:19:17 +0100

    try out averaging if h>0

--- ECMWF/pullTrack_CloudNet_ECMWF.py
+++ ECMWF/pullTrack_CloudNet_ECMWF.py
@@ -948,16 +948,20 @@ def writeNetCDF(data, date, outfile):
                 else:
                     if np.size(cube[d].shape) == 0:
                         print 'Diagnostic is a scalar field, so writing hour = ' + str(h)
-                        dat[:] = cube[d].data
+                        # dat[:] = cube[d].data
+                        dat[:] = np.nanmean([cube1[d].data,cube2[d].data])
                     elif np.size(cube[d].shape) == 1:
                         print 'Diagnostic is 1D, so writing hour = ' + str(h)
-                        dat[h] = cube[d].data[h]
+                        # dat[h] = cube[d].data[h]
+                        dat[h] = np.nanmean([cube1[d].data[h],cube2[d].data[h]])
                     elif np.size(cube[d].shape) == 2:
                         print 'Diagnostic is 2D, so writing hour = ' + str(h)
-                        dat[h,:] = cube[d].data[h,:]
+                        # dat[h,:] = cube[d].data[h,:]
+                        dat[h,:] = np.nanmean([cube1[d].data[h,:],cube2[d].data[h,:]],0)
                     elif np.size(cube[d].shape) == 3:
                         print 'Diagnostic is 3D, so writing hour = ' + str(h)
-                        dat[:,h,:] = cube[d].data[:,h,:]
+                        # dat[:,h,:] = cube[d].data[:,h,:]
+                        dat[:,h,:] = np.nanmean([cube[d].data[:,h,:],cube2[d].data[:,h,:]],1)
             dat.scale_factor = float(1)
             dat.add_offset = float(0)
             dat.units = str(cube[d].units)
