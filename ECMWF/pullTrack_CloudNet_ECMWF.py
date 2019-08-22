e1d16a9048d3233ece98c91e980535be59465708
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-22 17:17:20 +0100

    20180907

--- ECMWF/pullTrack_CloudNet_ECMWF.py
+++ ECMWF/pullTrack_CloudNet_ECMWF.py
@@ -1135,7 +1135,7 @@ def main():
     ### -------------------------------------------------------------------------
     ### define input filenames
     ### -------------------------------------------------------------------------
-    date = '20180906'
+    date = '20180907'
     outfile = date + '_oden_ecmwf.nc'
     print 'Outfile will be: ' + outfile
     base_name = date + '_moccha_ecmwf_'
