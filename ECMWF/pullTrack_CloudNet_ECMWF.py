c14b348aa6e3bda381b606351fe25a3892bb48c7
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-28 10:30:35 +0100

    seems like 20180913 was okay... not sure why it didn't run last time

--- ECMWF/pullTrack_CloudNet_ECMWF.py
+++ ECMWF/pullTrack_CloudNet_ECMWF.py
@@ -1144,7 +1144,7 @@ def main():
     ### -------------------------------------------------------------------------
     ### define input filenames
     ### -------------------------------------------------------------------------
-    date = '20180912'
+    date = '20180913'
     outfile = date + '_oden_ecmwf.nc'
     print 'Outfile will be: ' + outfile
     base_name = date + '_moccha_ecmwf_'
