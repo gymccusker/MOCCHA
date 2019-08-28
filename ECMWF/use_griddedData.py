47254f004b87930e73a790a3fd90e707c0336835
gyoung410 <G.Young1@leeds.ac.uk>
2019-08-28 11:40:15 +0100

    include 20180830

--- ECMWF/use_griddedData.py
+++ ECMWF/use_griddedData.py
@@ -1522,7 +1522,7 @@ def main():
             '20180818_oden_ecmwf.nc','20180819_oden_ecmwf.nc','20180820_oden_ecmwf.nc','20180821_oden_ecmwf.nc',
             '20180822_oden_ecmwf.nc','20180823_oden_ecmwf.nc','20180824_oden_ecmwf.nc','20180825_oden_ecmwf.nc',
             '20180826_oden_ecmwf.nc','20180827_oden_ecmwf.nc','20180828_oden_ecmwf.nc','20180829_oden_ecmwf.nc',
-            '20180831_oden_ecmwf.nc']
+            '20180830_oden_ecmwf.nc','20180831_oden_ecmwf.nc']
 
     Sep_names = ['20180901_oden_ecmwf.nc','20180902_oden_ecmwf.nc','20180903_oden_ecmwf.nc','20180904_oden_ecmwf.nc',
             '20180905_oden_ecmwf.nc','20180906_oden_ecmwf.nc','20180907_oden_ecmwf.nc','20180908_oden_ecmwf.nc',
@@ -1538,7 +1538,7 @@ def main():
             '20180908_oden_ecmwf.nc','20180909_oden_ecmwf.nc','20180910_oden_ecmwf.nc','20180911_oden_ecmwf.nc',
             '20180912_oden_ecmwf.nc','20180913_oden_ecmwf.nc']
 
-    Aug_missing_files = ['20180812_oden_ecmwf.nc','20180813_oden_ecmwf.nc','20180830_oden_ecmwf.nc']
+    Aug_missing_files = ['20180812_oden_ecmwf.nc','20180813_oden_ecmwf.nc']
 
     Sep_missing_files = ['20180914_oden_ecmwf.nc']
 
