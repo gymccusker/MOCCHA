779e21c1f9016468559d61cb7913c7d79362a8fa
gyoung410 <G.Young1@leeds.ac.uk>
2019-09-03 13:59:01 +0100

    filename needs to be adaptable depending on model

--- UM/UM-IFS_comparison.py
+++ UM/UM-IFS_comparison.py
@@ -1540,6 +1540,7 @@ def main():
 
     ### CHOSEN RUN
     out_dir1 = '4_u-bg610_RA2M_CON/OUT_R1/papbpc_combined/'
+    out_dir2 = 'OUT2/'
 
     ### TESTING/domain_tests/umnsaa_pa000
     ### 4_u-bg610_RA2M_CON/OUT_R1/papbpc_combined/
@@ -1584,28 +1585,27 @@ def main():
     ### define input filename
     ### -------------------------------------------------------------------------
     # tempnames = ['umnsaa_pa012_r0.nc','umnsaa_pb012_r0.nc','umnsaa_pc011_r0.nc','umnsaa_pd011_r0.nc','20180812_oden_metum.nc']
-    Aug_names = ['20180813_oden_metum.nc','20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
-            '20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
-            '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
-            '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
-            '20180829_oden_metum.nc','20180830_oden_metum.nc','20180831_oden_metum.nc']
+    Aug_names = ['20180813_oden_','20180814_oden_','20180815_oden_','20180816_oden_',
+            '20180817_oden_','20180818_oden_','20180819_oden_','20180820_oden_',
+            '20180821_oden_','20180822_oden_','20180823_oden_','20180824_oden_',
+            '20180825_oden_','20180826_oden_','20180827_oden_','20180828_oden_',
+            '20180829_oden_','20180830_oden_','20180831_oden_']
 
-    Sep_names = ['20180901_oden_metum.nc','20180902_oden_metum.nc','20180903_oden_metum.nc','20180904_oden_metum.nc',
-            '20180905_oden_metum.nc','20180906_oden_metum.nc','20180907_oden_metum.nc','20180908_oden_metum.nc',
-            '20180909_oden_metum.nc','20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc',
-            '20180913_oden_metum.nc','20180914_oden_metum.nc']
+    Sep_names = ['20180901_oden_','20180902_oden_','20180903_oden_','20180904_oden_',
+            '20180905_oden_','20180906_oden_','20180907_oden_','20180908_oden_',
+            '20180909_oden_','20180910_oden_','20180911_oden_','20180912_oden_',
+            '20180913_oden_','20180914_oden_']
 
-    moccha_names = ['20180813_oden_metum.nc','20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
-            '20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
-            '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
-            '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
-            '20180829_oden_metum.nc','20180830_oden_metum.nc','20180831_oden_metum.nc','20180901_oden_metum.nc',
-            '20180902_oden_metum.nc','20180903_oden_metum.nc','20180904_oden_metum.nc','20180905_oden_metum.nc',
-            '20180906_oden_metum.nc','20180907_oden_metum.nc','20180908_oden_metum.nc','20180909_oden_metum.nc',
-            '20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc',
-            '20180914_oden_metum.nc']
+    moccha_names = ['20180813_oden_','20180814_oden_','20180815_oden_','20180816_oden_',
+            '20180817_oden_','20180818_oden_','20180819_oden_','20180820_oden_',
+            '20180821_oden_','20180822_oden_','20180823_oden_','20180824_oden_',
+            '20180825_oden_','20180826_oden_','20180827_oden_','20180828_oden_',
+            '20180829_oden_','20180830_oden_','20180831_oden_','20180901_oden_',
+            '20180902_oden_','20180903_oden_','20180904_oden_','20180905_oden_',
+            '20180906_oden_','20180907_oden_','20180908_oden_','20180909_oden_',
+            '20180910_oden_','20180911_oden_','20180912_oden_','20180913_oden_','20180914_oden_']
 
-    Aug_missing_files = ['20180812_oden_metum.nc']
+    Aug_missing_files = []
 
     Sep_missing_files = []
 
@@ -1616,45 +1616,32 @@ def main():
     # names = ['umnsaa_pa000','umnsaa_pc000.nc']       ### DEFAULT OUTPUT NAMES FOR TESTING
 
     ## Flag for individual file or monthly:
-    combine = 1
+    combine = 0
     ## Choose month:
-    names = moccha_names
-    missing_files = moccha_missing_files
-    month_flag = -1
+    names = Aug_names
+    missing_files = Aug_missing_files
+    month_flag = 8
 
     if combine == 0:
-        # name = '20180902_oden_metum.nc'
-        filename = root_dir + out_dir + names[0]
-        print filename
+        name = '20180813_oden_'
+        filename_um = um_root_dir + out_dir1 + name + 'metum.nc'
+        filename_ifs = ifs_root_dir + out_dir2 + name+ 'ecmwf.nc'
+        print filename_um
+        print filename_ifs
         print ''
 
         #### LOAD CUBE
-        if 'var_con' in locals():
-            print 'Loading single diagnostic:'
-            print var_con
-            cube1 = iris.load_cube(filename)#, var_con, callback)
-        elif 'global_con' in locals():
-            print 'Loading multiple diagnostics:'
-            # cube = iris.load_cubes(filename1, global_con)
-            cube = iris.load(filename)#, global_con, callback)
-
+        print 'Loading UM diagnostics:'
+        cube_um = iris.load(filename_um)#, global_con, callback)
+        print '...'
+        print 'Loading IFS diagnostics:'
+        cube_ifs = iris.load(filename_ifs)#, global_con, callback)
             # -------------------------------------------------------------
 
-        print cube
+        print cube_um
         print ''
-
-        # -------------------------------------------------------------
-        # Plot data (5x2 timeseries)
-        # -------------------------------------------------------------
-        # figure = plot_multicontour_TS(cube, filename, out_dir)
-
-        # -------------------------------------------------------------
-        # Plot data (map)
-        # -------------------------------------------------------------
-        ### select hour to plot
-        # date = '20180814'
-        # hour = 0
-        # figure = plot_cartmap(ship_data, cube, hour, date)#, lon, lat)
+        print cube_ifs
+        print ''
 
     else:
         for i in range(0,len(names)):
