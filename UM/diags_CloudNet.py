9747e3debaa93cb982c658b187326302ad4b224f
gyoung410 <g.young1@leeds.ac.uk>
2019-10-08 09:42:22 +0100

    add moose command for run 7 (PC2 nest + global 3d swath)

--- UM/diags_CloudNet.py
+++ UM/diags_CloudNet.py
@@ -805,6 +805,7 @@ def main():
     #### moo select stash_extract_CASIM.p5 moose:crum/u-bl616/apm.pp 5_u-bl616_RA2M_CASIM/20180901T1200Z/
     #### moo select stash_extract_CASIM.p5 moose:crum/u-bl661/apm.pp 5_u-bl661_RA1M_CASIM/20180831T1200Z/
     #### moo select stash_extract_CASIM.p5 moose:crum/u-bm410/apm.pp 6_u-bm410_RA1M_CASIM/20180905T1200Z/
+    #### moo select stash_extract_all.p4 moose:crum/u-bn068/apm.pp 7_u-bn068_RA1M_CASIM/20180827T1200Z/
 
     ####    RUN SCRIPT IN BACKGROUND (change to executable with chmod +x diags_CloudNet.py)
     #### nohup python2.7 diags_CloudNet.py > nohup_u-bl661_diags_CloudNet.out &
