61735cb0bea9626a76689d8688d72d5c5951b0c5
gyoung410 <g.young1@leeds.ac.uk>
2019-11-22 09:24:49 +0000

    cartopy and iris not available on desktop

--- Cloudnet/use_cloudnetData.py
+++ Cloudnet/use_cloudnetData.py
@@ -13,8 +13,8 @@ import numpy as np
 import pandas as pd
 # import diags_MOCCHA as diags
 # import diags_varnames as varnames
-import cartopy.crs as ccrs
-import iris
+# import cartopy.crs as ccrs
+# import iris
 import matplotlib.pyplot as plt
 import matplotlib.cm as mpl_cm
 import os
