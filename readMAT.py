d2229f337e7ef9e08047da9b0b3da4c65ab74db0
gyoung410 <g.young1@leeds.ac.uk>
2019-12-02 15:34:05 +0000

    start readMAT script to load in .mat files containing structs

---
+++ readMAT.py
@@ -0,0 +1,13 @@
+"""
+Steps to read in Matlab struct files (saved as .mat)
+==============================
+
+"""
+
+from scipy.io import loadmat
+import numpy as np
+
+
+def readMAT(data, struct_name):
+
+    return mat      #### returns dictionary containing matlab struct
