d6de0d215b8a6f373b70786df3aa4cede9d2ec94
gyoung410 <g.young1@leeds.ac.uk>
2019-12-02 16:00:13 +0000

    add in some print statements

--- readMAT.py
+++ readMAT.py
@@ -13,12 +13,16 @@ def readMatlabStruct(filename, struct_name):
     #### --------------------------------------------------------------------
     #### LOAD MATLAB FILE USING SCIPY
     #### --------------------------------------------------------------------
+    print 'Reading in .mat file including struct...'
     dat = loadmat(filename)
+    print ''
 
     #### --------------------------------------------------------------------
     #### USE STRUCT_NAME TO DEFINE INTERMEDIATE STRUCT ARRAY
     #### --------------------------------------------------------------------
+    print 'Dealing with intermediate data assignments...''
     struct = dat[struct_name]
+    print ''
 
     #### --------------------------------------------------------------------
     #### IDENTIFY DATA AS FIRST ENTRY IN INTERMEDIATE STRUCT
@@ -29,5 +33,14 @@ def readMatlabStruct(filename, struct_name):
             #### commands don't quite work...). MATLAB structs come back as
             #### numpy structured arrays.
 
+    #### --------------------------------------------------------------------
+    #### CHANGE NUMPY STRUCTURED ARRAY TO DICTIONARY FOR EASE OF USE
+    #### --------------------------------------------------------------------
+    # dict = {}
+
+    print 'Finished! :)'
+    print 'Reading out ' + struct_name + ' struct within .mat file as defined output variable name'
+    print '[default = data]'
+    print ''
 
     return data      #### returns structured numpy array containing matlab struct
