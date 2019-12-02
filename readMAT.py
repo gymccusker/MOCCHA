b5a2cf6baafff5be9c47d4635641a6a850c83533
gyoung410 <g.young1@leeds.ac.uk>
2019-12-02 15:42:12 +0000

    input stages of struct read-in

--- readMAT.py
+++ readMAT.py
@@ -8,6 +8,21 @@ from scipy.io import loadmat
 import numpy as np
 
 
-def readMAT(data, struct_name):
+def readMAT(filename, struct_name):
 
-    return mat      #### returns dictionary containing matlab struct
+    #### --------------------------------------------------------------------
+    #### LOAD MATLAB FILE USING SCIPY
+    #### --------------------------------------------------------------------
+    dat = loadmat(filename)
+
+    #### --------------------------------------------------------------------
+    #### USE STRUCT_NAME TO DEFINE INTERMEDIATE STRUCT ARRAY
+    #### --------------------------------------------------------------------
+    struct = dat[struct_name]
+
+    #### --------------------------------------------------------------------
+    #### IDENTIFY DATA AS FIRST ENTRY IN INTERMEDIATE STRUCT
+    #### --------------------------------------------------------------------
+    data = struct[0,0]
+
+    return data      #### returns dictionary containing matlab struct
