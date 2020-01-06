b4db60a4b4527b3ebcc6ebe915715de2a7e89069
gyoung410 <g.young1@leeds.ac.uk>
2020-01-06 15:34:48 +0000

    update to combine new CASIM files

--- UM/diags_CloudNet.py
+++ UM/diags_CloudNet.py
@@ -785,7 +785,7 @@ def main():
         ship_filename = '/nfs/a96/MOCCHA/working/gillian/ship/2018_shipposition_1hour.txt'
 
     ### CHOSEN RUN
-    out_dir = '7_u-bn068_RA2T_CON/'
+    out_dir = '5_u-bl661_RA1M_CASIM/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 1_20160401_61DIAG_TEST/
