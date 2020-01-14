6787de90b3e3d21f2c34b923711f5ce7b8468a00
gyoung410 <G.Young1@leeds.ac.uk>
2020-01-14 14:49:45 +0000

    include 20180814

--- UM/adapt25H.py
+++ UM/adapt25H.py
@@ -366,8 +366,7 @@ def main():
             '20180909_oden_metum.nc','20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc',
             '20180913_oden_metum.nc','20180914_oden_metum.nc']
 
-    moccha_names = [#'20180813_oden_metum.nc','20180814_oden_metum.nc',
-            '20180815_oden_metum.nc','20180816_oden_metum.nc',
+    moccha_names = ['20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
             '20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
             '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
             '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
@@ -377,7 +376,7 @@ def main():
             '20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc',
             '20180914_oden_metum.nc']
 
-    Aug_missing_files = ['20180812_oden_metum.nc']
+    Aug_missing_files = ['20180812_oden_metum.nc','20180813_oden_metum.nc']
 
     Sep_missing_files = []
 
