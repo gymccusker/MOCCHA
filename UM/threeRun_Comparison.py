4d85874b3d022b7b229b1e173b87e791e78f2599
gyoung410 <G.Young1@leeds.ac.uk>
2020-01-15 11:51:06 +0000

    and as svg...

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -1113,7 +1113,7 @@ def plot_line_TSa(data1, data2, data3, month_flag, missing_files, out_dir1, out_
             elif out_dir4 == 'OUT_25H/':
                 fileout = '../FIGS/comparisons/' + out_dir2[:20] + '_oden_metum_ifs_casim-100_TSa.png'
         elif out_dir1[:18] == '4_u-bg610_RA2M_CON':
-            fileout = '../FIGS/comparisons/' + out_dir1[:9] + '_' + out_dir2[:9] +'_oden_metum_casim-100_TSa.png'
+            fileout = '../FIGS/comparisons/' + out_dir1[:9] + '_' + out_dir2[:9] +'_oden_metum_casim-100_TSa.svg'
     # fileout = '../FIGS/comparisons/' + out_dir1[:9] + '_' + out_dir2[:9] + '_oden_metum_ra2t_ifs_TSa.png'
     plt.savefig(fileout, dpi=300)
     plt.show()
