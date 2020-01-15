73935060ac7a350a6fe2bc96bccb633a6a016a0c
gyoung410 <G.Young1@leeds.ac.uk>
2020-01-15 14:42:03 +0000

    test plot rad

--- UM/use_griddedData.py
+++ UM/use_griddedData.py
@@ -2059,7 +2059,7 @@ def main():
         ship_filename = '~/GWS/MOCCHA/ODEN/2018_shipposition_1hour.txt'
     if platform == 'LAPTOP':
         root_dir = '/home/gillian/MOCCHA/UM/DATA/'
-        obs_root_dir = '/home/gillian/MOCCHA/ODEN/'
+        obs_root_dir = '/home/gillian/MOCCHA/ODEN/DATA/'
         ship_filename = '/home/gillian/MOCCHA/ODEN/DATA/2018_shipposition_1hour.txt'
     if platform == 'MONSOON':
         root_dir = '~/cylc-run/u-bg610/share/cycle/20160401T0000Z/HighArctic/1p5km/RA2M_CON/um/'
@@ -2156,11 +2156,11 @@ def main():
     moccha_names = [#'20180813_oden_metum.nc','20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
             # '20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
             # '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
-            # '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc',
-            '20180828_oden_metum.nc',
-            '20180829_oden_metum.nc','20180830_oden_metum.nc','20180831_oden_metum.nc','20180901_oden_metum.nc',
-            '20180902_oden_metum.nc','20180903_oden_metum.nc','20180904_oden_metum.nc','20180905_oden_metum.nc',
-            '20180906_oden_metum.nc','20180907_oden_metum.nc']#,'20180908_oden_metum.nc','20180909_oden_metum.nc',
+            # '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
+            # '20180829_oden_metum.nc',
+            '20180830_oden_metum.nc','20180831_oden_metum.nc','20180901_oden_metum.nc',
+            '20180902_oden_metum.nc','20180903_oden_metum.nc','20180904_oden_metum.nc']#,'20180905_oden_metum.nc',
+            # '20180906_oden_metum.nc','20180907_oden_metum.nc','20180908_oden_metum.nc','20180909_oden_metum.nc',
             # '20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc',
             # '20180914_oden_metum.nc']
 
@@ -2172,8 +2172,8 @@ def main():
 
     # doy = np.arange(225,258)        ## set DOY for full moccha figures
     # doy = np.arange(240,244)        ## set DOY for subset of moccha figures
-    # doy = np.arange(243,249)        ## set DOY for subset of moccha figures
-    doy = np.arange(240,251)        ## set DOY for subset of moccha figures
+    doy = np.arange(243,249)        ## set DOY for subset of moccha figures
+    # doy = np.arange(240,251)        ## set DOY for subset of moccha figures
 
     # names = ['umnsaa_pa000','umnsaa_pc000.nc']       ### DEFAULT OUTPUT NAMES FOR TESTING
 
