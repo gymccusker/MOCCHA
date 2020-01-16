3583cbb19a37f2b14cd6b7af3c43b435e894586b
gyoung410 <G.Young1@leeds.ac.uk>
2020-01-16 09:46:36 +0000

    plot latent heat flux too

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -1201,7 +1201,8 @@ def plot_line_Fluxes(data1, data2, data3, month_flag, missing_files, out_dir1, o
         plt.plot(data3['time'], data3['sfc_down_lat_heat_flx'].data * -1.0, color = 'darkorange')
     else:
         plt.plot(data3['time'], data3['latent_heat_flux'].data, color = 'darkorange')# * -1.0)
-    plt.plot(foremast.variables['doy'][foremast.variables['rflag'][:] == 1], foremast.variables['rflux'][foremast.variables['rflag'][:] == 1], 'k.')
+    plt.plot(foremast.variables['doy'][foremast.variables['rflag'][:] == 1], foremast.variables['rflux'][foremast.variables['rflag'][:] == 1], 'k.', label = 'Foremast')
+    plt.plot(time_iceStation,ice_station['rflux'], '.', color = 'grey', label = 'Ice_station')
     plt.title('latent_heat_flux [W/m2]')
     ax.set_xlim([doy[0],doy[-1]])
     plt.xlabel('Day of year')
