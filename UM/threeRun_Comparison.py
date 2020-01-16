b6a5774df8a301bc9abbb486bfa44521ed349d9a
gyoung410 <G.Young1@leeds.ac.uk>
2020-01-16 10:02:27 +0000

    try manually constraing LHF for now until I chat with Jutta about flags

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -1185,8 +1185,8 @@ def plot_line_Fluxes(data1, data2, data3, month_flag, missing_files, out_dir1, o
         plt.plot(data3['time'], data3['sfc_down_sens_heat_flx'].data * -1.0, color = 'darkorange')
     else:
         plt.plot(data3['time'], data3['sensible_heat_flux'].data, color = 'darkorange')# * -1.0)
-    plt.plot(foremast.variables['doy'][foremast.variables['taflag'][:] == 1], foremast.variables['taflux'][foremast.variables['taflag'][:] == 1], 'kd', markersize = 4, label = 'Foremast')
-    plt.plot(time_iceStation,ice_station['taflux'], 's', color = 'grey', markersize = 4, label = 'Ice_station')
+    plt.plot(foremast.variables['doy'][foremast.variables['taflag'][:] == 1], foremast.variables['taflux'][foremast.variables['taflag'][:] == 1], 'kd', markersize = 3, label = 'Foremast')
+    plt.plot(time_iceStation,ice_station['taflux'], 's', color = 'grey', markersize = 3, label = 'Ice_station')
     plt.legend()
     plt.ylim([-30, 30])
     plt.title('sensible_heat_flux [W/m2]')
@@ -1201,8 +1201,9 @@ def plot_line_Fluxes(data1, data2, data3, month_flag, missing_files, out_dir1, o
         plt.plot(data3['time'], data3['sfc_down_lat_heat_flx'].data * -1.0, color = 'darkorange')
     else:
         plt.plot(data3['time'], data3['latent_heat_flux'].data, color = 'darkorange')# * -1.0)
-    plt.plot(foremast.variables['doy'][foremast.variables['rflag'][:] == 1], foremast.variables['rflux'][foremast.variables['rflag'][:] == 1], 'kd', markersize = 4, label = 'Foremast')
-    plt.plot(time_iceStation,ice_station['lrflux_hc'], 's', color = 'grey', markersize = 4, label = 'Ice_station')
+    plt.plot(foremast.variables['doy'][foremast.variables['rflag'][:] == 1], foremast.variables['rflux'][foremast.variables['rflag'][:] == 1], 'kd', markersize = 3, label = 'Foremast')
+    index = np.logical_and(ice_station['lrflux']>=-50, ice_station['lrflux']<=100)
+    plt.plot(time_iceStation,np.squeeze(ice_station['lrflux'][index]), 's', color = 'grey', markersize = 3, label = 'Ice_station')
     plt.title('latent_heat_flux [W/m2]')
     ax.set_xlim([doy[0],doy[-1]])
     plt.xlabel('Day of year')
