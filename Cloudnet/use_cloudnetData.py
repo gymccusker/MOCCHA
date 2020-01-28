9f90ec5a6c807a5641f072036c9e0bfc506eda86
gyoung410 <G.Young1@leeds.ac.uk>
2020-01-28 09:42:33 +0000

    fix axes

--- Cloudnet/use_cloudnetData.py
+++ Cloudnet/use_cloudnetData.py
@@ -148,7 +148,7 @@ def plot_LWP(um_data, ifs_data, misc_data, obs_data, month_flag, missing_files, 
     plt.rc('ytick',labelsize=LARGE_SIZE)
     plt.rc('legend',fontsize=LARGE_SIZE)
     plt.figure(figsize=(6,8))
-    plt.subplots_adjust(top = 0.9, bottom = 0.1, right = 0.96, left = 0.2,
+    plt.subplots_adjust(top = 0.9, bottom = 0.1, right = 0.96, left = 0.1,
             hspace = 0.4, wspace = 0.1)
 
     ### define axis instance
