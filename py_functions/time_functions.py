653d6aaf386b8f60bd510f7a5d42eb0aaae61f9e
gyoung410 <G.Young1@leeds.ac.uk>
2020-01-29 14:29:49 +0000

    add function to calculate DOY from date (YYYYmmDD). ONLY VALID FOR MOCCHA DRIFT PERIOD!!

--- py_functions/time_functions.py
+++ py_functions/time_functions.py
@@ -20,3 +20,24 @@ def calcTime_Mat2DOY(matlab_time):
     python_time = timestamps.dayofyear + (timestamps.hour / 24.0) + (timestamps.minute / 1440.0) + (timestamps.second / 86400.0)
 
     return python_time
+
+def calcTime_Date2DOY(date):
+
+    ####        date should be forematted as YYYYmmDD
+
+    print 'Converting date to DOY:'
+
+    mm = date[4:6]      #### month
+    DD = date[6:8]      #### day
+    refDateAug = 226       #### Aug reference date for drift: 14th Aug 2018
+    refDateSep = 243       #### Sep reference date for drift: 1st Sep 2018
+
+    if mm == '08':
+        doy = (float(DD) - 14.0) + refDateAug
+    elif mm == '09':
+        doy = float(DD) + refDateSep
+    else:
+        print '****Date not valid with this function****'
+
+
+    return doy
