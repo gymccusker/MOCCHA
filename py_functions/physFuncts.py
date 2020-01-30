b73f4573cc0707efa7ef373cb19443fa8bd2a055
gyoung410 <G.Young1@leeds.ac.uk>
2020-01-30 12:14:06 +0000

    should just reference (2D) temperature, indexed at function call

--- py_functions/physFuncts.py
+++ py_functions/physFuncts.py
@@ -20,8 +20,8 @@ def calcAirDensity(temperature, pressure):
     R = 2.8704  #### hPa kg-1 K-1
 
     print 'Calculating air density profile:'
-    rho = np.zeros([np.size(um.variables['temperature'],0),np.size(um.variables['temperature'],1)])
-    for k in range(0,np.size(um.variables['temperature'],1)):
+    rho = np.zeros([np.size(temperature,0),np.size(temperature,1)])
+    for k in range(0,np.size(temperature,1)):
         rho[:,k] = pressure[:,k] / (R * temperature[:,k])
 
     return rho
