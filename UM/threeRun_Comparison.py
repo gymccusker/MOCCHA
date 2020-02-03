thetaE calculation fixed

UM/threeRun_Comparison.py
@@ -2887,7 +2887,7 @@ def plot_RadiosondesThetaE(data1, data2, data3, month_flag, missing_files, out_d
     data3['theta'], data3['thetaE'] = calcThetaE(data3['temperature'], data3['pressure'], data3['q'], data3['time'], np.squeeze(data3['height'][0,:]))
 
     obs['sondes']['theta'], obs['sondes']['thetaE'] = calcThetaE(np.transpose(obs['sondes']['temperature'])+273.15,
-        np.transpose(obs['sondes']['pressure'])*1e2, np.transpose(obs['sondes']['mr']),
+        np.transpose(obs['sondes']['pressure'])*1e2, np.transpose(obs['sondes']['mr'])/1e3,
         obs['sondes']['doy'], obs['sondes']['gpsaltitude'])
 
     obs['sondes']['thetaE'] = np.transpose(obs['sondes']['thetaE'])         ### for consistency with original sonde dimensions