17c59deb0fdf91a0cbf7f8767b4b7df5c392d3c7
GYoung410 <gyoung90.gy@gmail.com>
2020-02-11 13:48:14 +0000

    interpolation now fixed for IFS. Sonde has some discrepancy >8km, but not sure if there's much we can do about that... unless we regrid sonde to ifs?!

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -2992,21 +2992,21 @@ def reGrid_Sondes(data1, data2, data3, obs, doy, var):
     obs['sondes']['temp_driftSondes_UM'] = obs['sondes']['temp_allSondes_UM'][drift[0],:]
 
     #### INTERPOLATION TESTING - IFS + SONDE + UM_RA2M:
-    print (obs['sondes']['doy_drift'].shape)
-    print (obs['sondes']['temp_allSondes_UM'][drift[0],:].shape)
-    for i in range(0, np.size(obs['sondes']['doy_drift'])):
-        plt.plot(np.squeeze(obs['sondes']['temperature'][iObs,drift[0][i]]) + 273.15,np.squeeze(obs['sondes']['gpsaltitude'][iObs,drift[0][i]]), '--', color = 'k', label = 'sonde-original')
-        plt.plot(obs['sondes']['temp_driftSondes_UM'][i,:] + 273.15,data1['height'][iUM[0][3:]], color = 'k', label = 'sonde-interpd')
-        plt.plot(np.squeeze(data3['temp_6hrly'][i,iIFS]),np.squeeze(data3['height_6hrly'][i,iIFS]), '--', color = 'darkorange', label = 'ifs-Zindexed')
-        plt.plot(data3['temp_6hrly_UM'][i,:],data1['height'][iUM[0][3:]], color = 'darkorange', label = 'ifs-interpd')
-        plt.plot(data1['temp_6hrly'][i,iUM[0][3:]], data1['height'][iUM[0][3:]], color = 'steelblue', label = 'um_ra2m')
-        plt.title('REGRID test ' + str(np.round(obs['sondes']['doy_drift'][i],2)))
-        plt.legend()
-        plt.savefig('../FIGS/regrid/REGRID_test_doy' + str(np.round(obs['sondes']['doy_drift'][i],1)) + '.png')
-        if i == 0:
-            plt.show()
-        else:
-            plt.close()
+    # print (obs['sondes']['doy_drift'].shape)
+    # print (obs['sondes']['temp_allSondes_UM'][drift[0],:].shape)
+    # for i in range(0, np.size(obs['sondes']['doy_drift'])):
+    #     plt.plot(np.squeeze(obs['sondes']['temperature'][iObs,drift[0][i]]) + 273.15,np.squeeze(obs['sondes']['gpsaltitude'][iObs,drift[0][i]]), '--', color = 'k', label = 'sonde-original')
+    #     plt.plot(obs['sondes']['temp_driftSondes_UM'][i,:] + 273.15,data1['height'][iUM[0][3:]], color = 'k', label = 'sonde-interpd')
+    #     plt.plot(np.squeeze(data3['temp_6hrly'][i,iIFS]),np.squeeze(data3['height_6hrly'][i,iIFS]), '--', color = 'darkorange', label = 'ifs-Zindexed')
+    #     plt.plot(data3['temp_6hrly_UM'][i,:],data1['height'][iUM[0][3:]], color = 'darkorange', label = 'ifs-interpd')
+    #     plt.plot(data1['temp_6hrly'][i,iUM[0][3:]], data1['height'][iUM[0][3:]], color = 'steelblue', label = 'um_ra2m')
+    #     plt.title('REGRID test ' + str(np.round(obs['sondes']['doy_drift'][i],2)))
+    #     plt.legend()
+    #     plt.savefig('../FIGS/regrid/REGRID_test_doy' + str(np.round(obs['sondes']['doy_drift'][i],1)) + '.png')
+    #     if i == 0:
+    #         plt.show()
+    #     else:
+    #         plt.close()
 
     #### ---------------------------------------------------------------
     #### make some dictionary assignments for use later
