7c501bdbe516de9d7939310c183d28994a5371ef
gyoung410 <G.Young1@leeds.ac.uk>
2020-02-13 14:51:15 +0000

    fix dims of melt/freeze profiles

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -2391,8 +2391,8 @@ def plot_RadiosondesTemperature(data1, data2, data3, month_flag, missing_files, 
     fig = plt.figure(figsize=(8,5))
 
     ####        all model data share a timestamp
-    melt = np.where(data1['time_hrly'] < 240.0)
-    freeze = np.where(data1['time_hrly'] >= 240.0)
+    melt = np.where(data1['time_hrly'][::6] < 240.0)
+    freeze = np.where(data1['time_hrly'][::6] >= 240.0)
 
     plt.subplot(131)
     plt.plot(np.nanmedian(data1['temp_anomalies'],1),data1['universal_height'],label = 'UM_RA2M median')
@@ -2403,18 +2403,19 @@ def plot_RadiosondesTemperature(data1, data2, data3, month_flag, missing_files, 
     plt.grid('on')
 
     plt.subplot(132)
-    plt.plot(np.nanmedian(data1['temp_anomalies'][:,melt],1),data1['universal_height'],label = 'UM_RA2M median')
-    plt.plot(np.nanmedian(data2['temp_anomalies'][:,melt],1),data1['universal_height'],label = 'UM_CASIM-100 median')
-    plt.plot(np.nanmedian(data3['temp_anomalies'][:,melt],1),data1['universal_height'],label = 'ECMWF_IFS median')
+    plt.plot(np.nanmedian(np.squeeze(data1['temp_anomalies'][:,melt]),1),data1['universal_height'],label = 'UM_RA2M median')
+    plt.plot(np.nanmedian(np.squeeze(data2['temp_anomalies'][:,melt]),1),data1['universal_height'],label = 'UM_CASIM-100 median')
+    plt.plot(np.nanmedian(np.squeeze(data3['temp_anomalies'][:,melt]),1),data1['universal_height'],label = 'ECMWF_IFS median')
     plt.grid('on')
     plt.ylim([0,1e4])
 
     plt.subplot(133)
-    plt.plot(np.nanmedian(data1['temp_anomalies'][:,freeze],1),data1['universal_height'],label = 'UM_RA2M median')
-    plt.plot(np.nanmedian(data2['temp_anomalies'][:,freeze],1),data1['universal_height'],label = 'UM_CASIM-100 median')
-    plt.plot(np.nanmedian(data3['temp_anomalies'][:,freeze],1),data1['universal_height'],label = 'ECMWF_IFS median')
+    plt.plot(np.nanmedian(np.squeeze(data1['temp_anomalies'][:,freeze]),1),data1['universal_height'],label = 'UM_RA2M median')
+    plt.plot(np.nanmedian(np.squeeze(data2['temp_anomalies'][:,freeze]),1),data1['universal_height'],label = 'UM_CASIM-100 median')
+    plt.plot(np.nanmedian(np.squeeze(data3['temp_anomalies'][:,freeze]),1),data1['universal_height'],label = 'ECMWF_IFS median')
     plt.grid('on')
     plt.ylim([0,1e4])
+    plt.show()
 
     # height = np.zeros([np.size(um['temp_anomalies'],0), np.size(um['temp_anomalies'],1)])
     # for i in range(0,np.size(height,1)): height[:,i] = um['universal_height']
