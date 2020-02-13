2992c5f8afd1ca6659a736c1904c61e4e508db9d
gyoung410 <G.Young1@leeds.ac.uk>
2020-02-13 13:43:10 +0000

    split scatters by season, doing individual figs for supplement

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -1737,6 +1737,16 @@ def plot_BLDepth(data1, data2, data3, month_flag, missing_files, out_dir1, out_d
     ### save in dict for ease
     obs['inversions']['doy_drift'] = obs['inversions']['doy'][drift]
 
+    #### split into melt and freeze in scatter plots:
+    ####        all model data share a timestamp
+    melt = np.where(np.logical_and(data1['time_hrly'] >= obs['inversions']['doy_drift'][0], data1['time_hrly'] < 240.0))
+    freeze = np.where(data1['time_hrly'] >= 240.0)
+    obsmelt = np.squeeze(np.where(obs['inversions']['doy_drift'] < 240.0))
+    obsfreeze = np.squeeze(np.where(obs['inversions']['doy_drift'] >= 240.0))
+
+    print ('obsmelt.shape = ', obsmelt.shape)
+
+    #### make inversion tempvars to allow for easy subsampling
     inv1 = np.squeeze(data1['inversions']['invbase'][data1['hrly_flag'],0])
     inv2 = np.squeeze(data2['inversions']['invbase'][data2['hrly_flag'],0])
     inv3 = np.squeeze(data3['inversions']['invbase'][data3['hrly_flag'],0])
@@ -1761,70 +1771,119 @@ def plot_BLDepth(data1, data2, data3, month_flag, missing_files, out_dir1, out_d
     ### -------------------------------
     ### Build figure (timeseries)
     ### -------------------------------
-    fig = plt.figure(figsize=(10,7))
+    fig = plt.figure(figsize=(13,7))
 
     #################################################################
     ## create figure and axes instances
     #################################################################
-    ax  = fig.add_axes([0.1,0.56,0.58,0.36])   # left, bottom, width, height
+    ax  = fig.add_axes([0.08,0.56,0.45,0.36])   # left, bottom, width, height
     plt.plot(np.squeeze(obs['inversions']['doy_drift']),np.squeeze(obs['inversions']['sfmlheight'][drift]),
         color = 'k', label = 'Obs_Radiosondes')
     plt.plot(data1['time_hrly'][::6], bldepth1[::6],
         '^', color = 'steelblue', markeredgecolor = 'midnightblue', label = label1)
-    plt.plot(data2['time_hrly'][::6], bldepth2[::6],
-        'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
-    plt.plot(data3['time_hrly'][::6], bldepth3[::6],
-        'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
-    plt.legend()
+    # plt.plot(data2['time_hrly'][::6], bldepth2[::6],
+    #     'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
+    # plt.plot(data3['time_hrly'][::6], bldepth3[::6],
+    #     'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
+    plt.legend(bbox_to_anchor=(0.0, 0.67, 1., .102), loc=4, ncol=2)
     plt.title('BL_depth / sfmlheight [m]')
     ax.set_xlim([doy[0],doy[-1]])
     # plt.xlabel('Day of year')
     plt.ylabel('Z [m]')
 
-    ax  = fig.add_axes([0.1,0.1,0.58,0.36])   # left, bottom, width, height
+    ax  = fig.add_axes([0.08,0.1,0.45,0.36])   # left, bottom, width, height
     plt.plot(np.squeeze(obs['inversions']['doy_drift']), np.squeeze(obs['inversions']['invbase'][drift]),
         color = 'k', label = 'Obs: main inversion')
     plt.plot(data1['time_hrly'][::6], inv1[::6],
         '^', color = 'steelblue', markeredgecolor = 'midnightblue', label = label1)
-    plt.plot(data2['time_hrly'][::6], inv2[::6],
-        'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
-    plt.plot(data1['time_hrly'][::6], inv3[::6],
-        'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
+    # plt.plot(data2['time_hrly'][::6], inv2[::6],
+    #     'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
+    # plt.plot(data1['time_hrly'][::6], inv3[::6],
+    #     'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
     # plt.legend()
     plt.title('Main inversion height [m]')
     ax.set_xlim([doy[0],doy[-1]])
     plt.xlabel('Day of year')
     plt.ylabel('Z [m]')
 
-    ax  = fig.add_axes([0.79,0.59,0.18,0.3])   # left, bottom, width, height
-    ymax1 = 1500
+    #### -----------------------------------------------------------------
+    #### scatterplots
+    #### -----------------------------------------------------------------
+    ax  = fig.add_axes([0.6,0.64,0.15,0.22])   # left, bottom, width, height
+    ymax1 = 750
     xmax1 = ymax1
+    blmelt1 = bldepth1[melt]
+    blmelt2 = bldepth2[melt]
+    blmelt3 = bldepth3[melt]
     plt.plot([0,xmax1],[0, ymax1], '--', color = 'lightgrey')
-    plt.plot(np.squeeze(obs['inversions']['sfmlheight'][drift]), bldepth1[::6],
+    plt.plot(np.squeeze(obs['inversions']['sfmlheight'][0,obsmelt]), blmelt1[::6],
         '^', color = 'steelblue', markeredgecolor = 'midnightblue', label = label1)
-    plt.plot(np.squeeze(obs['inversions']['sfmlheight'][drift]), bldepth2[::6],
-        'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
-    plt.plot(np.squeeze(obs['inversions']['sfmlheight'][drift]), bldepth3[::6],
-        'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
+    # plt.plot(np.squeeze(obs['inversions']['sfmlheight'][0,obsmelt]), blmelt2[::6],
+    #     'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
+    # plt.plot(np.squeeze(obs['inversions']['sfmlheight'][0,obsmelt]), blmelt3[::6],
+    #     'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
     plt.xlim([0,xmax1])
     plt.ylim([0,ymax1])
     plt.xlabel('Obs$_{SML}$ [m]')
     plt.ylabel('Model$_{SML}$ [m]')
+    plt.title('Melt')
 
-    ax  = fig.add_axes([0.79,0.13,0.18,0.3])   # left, bottom, width, height
-    ymax2 = 2700
-    xmax2 = ymax2
-    plt.plot([0,xmax2],[0, ymax2], '--', color = 'lightgrey')
-    plt.plot(np.squeeze(obs['inversions']['invbase'][drift]), inv1[::6],
+    ax  = fig.add_axes([0.83,0.64,0.15,0.22])   # left, bottom, width, height
+    ymax1 = 1500
+    xmax1 = ymax1
+    blfreeze1 = bldepth1[freeze]
+    blfreeze2 = bldepth2[freeze]
+    blfreeze3 = bldepth3[freeze]
+    plt.plot([0,xmax1],[0, ymax1], '--', color = 'lightgrey')
+    plt.plot(np.squeeze(obs['inversions']['sfmlheight'][0,obsfreeze]), blfreeze1[::6],
         '^', color = 'steelblue', markeredgecolor = 'midnightblue', label = label1)
-    plt.plot(np.squeeze(obs['inversions']['invbase'][drift]), inv2[::6],
-        'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
-    plt.plot(np.squeeze(obs['inversions']['invbase'][drift]), inv3[::6],
-        'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
-    plt.xlim([0,xmax2])
-    plt.ylim([0,ymax2])
+    # plt.plot(np.squeeze(obs['inversions']['sfmlheight'][0,obsfreeze]), blfreeze2[::6],
+    #     'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
+    # plt.plot(np.squeeze(obs['inversions']['sfmlheight'][0,obsfreeze]), blfreeze3[::6],
+    #     'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
+    plt.xlim([0,xmax1])
+    plt.ylim([0,ymax1])
+    plt.xlabel('Obs$_{SML}$ [m]')
+    plt.ylabel('Model$_{SML}$ [m]')
+    plt.title('Freeze')
+
+    ax  = fig.add_axes([0.6,0.18,0.15,0.22])   # left, bottom, width, height
+    ymax1 = 2700
+    xmax1 = ymax1
+    invmelt1 = inv1[melt]
+    invmelt2 = inv2[melt]
+    invmelt3 = inv3[melt]
+    plt.plot([0,xmax1],[0, ymax1], '--', color = 'lightgrey')
+    plt.plot(np.squeeze(obs['inversions']['invbase'][0,obsmelt]), invmelt1[::6],
+        '^', color = 'steelblue', markeredgecolor = 'midnightblue', label = label1)
+    # plt.plot(np.squeeze(obs['inversions']['invbase'][0,obsmelt]), invmelt2[::6],
+    #     'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
+    # plt.plot(np.squeeze(obs['inversions']['invbase'][0,obsmelt]), invmelt3[::6],
+    #     'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
+    plt.xlim([0,xmax1])
+    plt.ylim([0,ymax1])
     plt.xlabel('Obs$_{inv}$ [m]')
     plt.ylabel('Model$_{inv}$ [m]')
+    plt.title('Melt')
+
+    ax  = fig.add_axes([0.83,0.18,0.15,0.22])   # left, bottom, width, height
+    ymax1 = 2700
+    xmax1 = ymax1
+    invfreeze1 = inv1[freeze]
+    invfreeze2 = inv2[freeze]
+    invfreeze3 = inv3[freeze]
+    plt.plot([0,xmax1],[0, ymax1], '--', color = 'lightgrey')
+    plt.plot(np.squeeze(obs['inversions']['invbase'][0,obsfreeze]), invfreeze1[::6],
+        '^', color = 'steelblue', markeredgecolor = 'midnightblue', label = label1)
+    # plt.plot(np.squeeze(obs['inversions']['invbase'][0,obsfreeze]), invfreeze2[::6],
+    #     'v', color = 'forestgreen', markeredgecolor = 'darkslategrey', label = label2)
+    # plt.plot(np.squeeze(obs['inversions']['invbase'][0,obsfreeze]), invfreeze3[::6],
+    #     'd', color = 'darkorange', markeredgecolor = 'saddlebrown',  label = label3)
+    plt.xlim([0,xmax1])
+    plt.ylim([0,ymax1])
+    plt.xlabel('Obs$_{inv}$ [m]')
+    plt.ylabel('Model$_{inv}$ [m]')
+    plt.title('Freeze')
 
 
     print ('******')
@@ -1832,8 +1891,8 @@ def plot_BLDepth(data1, data2, data3, month_flag, missing_files, out_dir1, out_d
     print ('Finished plotting! :)')
     print ('')
 
-    fileout = '../FIGS/comparisons/BLDepth_calcInvHeights_timeseries_wScatter_oden_metum_ifs_casim-100.svg'
-    # fileout = '../FIGS/comparisons/BLDepth_calcInvHeights_timeseries_wScatter_ifs.svg'
+    # fileout = '../FIGS/comparisons/BLDepth_calcInvHeights_timeseries_wScatter-SplitSeason_oden_metum_ifs_casim-100.svg'
+    fileout = '../FIGS/comparisons/BLDepth_calcInvHeights_timeseries_wScatter-SplitSeason_metum.svg'
     plt.savefig(fileout)
     plt.show()
 
@@ -3692,8 +3751,8 @@ def main():
     # -------------------------------------------------------------
     # figure = plot_paperFluxes(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
     # figure = plot_paperRadiation(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
-    figure = plot_Precipitation(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
-    # figure = plot_BLDepth(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
+    # figure = plot_Precipitation(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
+    figure = plot_BLDepth(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
     # figure = plot_BLType(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
     # figure = plot_RadiosondesTemperature(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
     # figure = plot_RadiosondesQ(data1, data2, data3, month_flag, missing_files, out_dir1, out_dir2, out_dir4, obs, doy, label1, label2, label3)
