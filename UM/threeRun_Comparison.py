366a2933cce89259e8a50727704cc58f5d2f5637
gyoung410 <G.Young1@leeds.ac.uk>
2020-02-27 17:51:16 +0000

    run for ra2t case

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -3573,7 +3573,7 @@ def main():
     ### CHOSEN RUN
     if platform == 'LAPTOP':
         out_dir1 = '4_u-bg610_RA2M_CON/OUT_R1/'
-        out_dir2 = '8_u-bp738_RA2M_CON/OUT_R0/'
+        out_dir2 = '7_u-bn068_RA2T_CON/OUT_R0/'
         # out_dir3 = 'MET_DATA/'
         out_dir4 = 'OUT_25H/'
     elif platform == 'JASMIN':
@@ -3675,10 +3675,10 @@ def main():
     moccha_names = [#'20180814_oden_','20180815_oden_','20180816_oden_',
             # '20180817_oden_','20180818_oden_','20180819_oden_','20180820_oden_',
             # '20180821_oden_','20180822_oden_','20180823_oden_','20180824_oden_',
-            # '20180825_oden_','20180826_oden_','20180827_oden_','20180828_oden_',
-            # '20180829_oden_','20180830_oden_',
-            '20180831_oden_','20180901_oden_',
-            '20180902_oden_','20180903_oden_','20180904_oden_','20180905_oden_']#,
+            # '20180825_oden_','20180826_oden_','20180827_oden_',
+            '20180828_oden_',
+            '20180829_oden_','20180830_oden_','20180831_oden_','20180901_oden_',
+            '20180902_oden_','20180903_oden_','20180904_oden_']#,'20180905_oden_',
             # '20180906_oden_','20180907_oden_','20180908_oden_','20180909_oden_',
             # '20180910_oden_','20180911_oden_','20180912_oden_','20180913_oden_','20180914_oden_']
 
@@ -3690,8 +3690,8 @@ def main():
 
     # doy = np.arange(226,259)        ## set DOY for full drift figures (over which we have cloudnet data)
     # doy = np.arange(240,251)        ## set DOY for subset of drift figures (presentations)
-    # doy = np.arange(240,248)        ## set DOY for RA2T  (28th Aug to 4th Sep)
-    doy = np.arange(243,250)        ## set DOY for ERAI-GLM  (31st Aug to 5th Sep)
+    doy = np.arange(240,248)        ## set DOY for RA2T  (28th Aug to 4th Sep)
+    # doy = np.arange(243,250)        ## set DOY for ERAI-GLM  (31st Aug to 5th Sep)
     # doy = np.arange(243,259)        ## set DOY for CASIM-AeroProf (31st Aug to 14th Sep)
 
     # names = ['umnsaa_pa000','umnsaa_pc000.nc']       ### DEFAULT OUTPUT NAMES FOR TESTING
