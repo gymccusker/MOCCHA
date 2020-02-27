10c861b6a8cc95dd8122549b3b01992fc68dbfd3
gyoung410 <G.Young1@leeds.ac.uk>
2020-02-27 20:10:01 +0000

    set up for ra2m, casim-100_AP, and ifs comparison

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -3579,7 +3579,7 @@ def main():
         out_dir1 = '4_u-bg610_RA2M_CON/OUT_R1/'
         out_dir2 = '13_u-br409_RA1M_CASIM/OUT_24h/'
         # out_dir3 = 'MET_DATA/'
-        out_dir4 = '5_u-bl661_RA1M_CASIM/OUT_R0/'
+        out_dir4 = 'OUT_25H/'
     elif platform == 'JASMIN':
         out_dir1 = 'UM_RA2M/'
         out_dir2 = 'UM_CASIM-100/'
