1d302a3feffd0414b740157a3ead4c9e0ee69ba1
gyoung410 <G.Young1@leeds.ac.uk>
2020-03-10 14:29:12 +0000

    add labels to temp figs

--- Cloudnet/cloudnetModel_Comparisons.py
+++ Cloudnet/cloudnetModel_Comparisons.py
@@ -865,20 +865,25 @@ def plot_scaledBL(data1, data2, data3, um_data, ifs_data, misc_data, obs_data, m
 
     ### timeseries
     plt.subplot(411)
+    plt.title('Obs')
     plt.pcolor(obs['inversions']['scaledTime'],obs['inversions']['scaledZ'],np.transpose(obs['inversions']['scaledCv']['mean']), vmin = 0, vmax = 1)
     plt.subplot(412)
+    plt.title(label1)
     plt.pcolor(data1['scaledTime'][::6],data1['scaledZ'],np.transpose(data1['scaledCv']['mean'][::6,:]), vmin = 0, vmax = 1)
     plt.subplot(413)
+    plt.title(label2)
     plt.pcolor(data2['scaledTime'][::6],data2['scaledZ'],np.transpose(data2['scaledCv']['mean'][::6,:]), vmin = 0, vmax = 1)
     plt.subplot(414)
+    plt.title(label3)
     plt.pcolor(data3['scaledTime'][::6],data3['scaledZ'],np.transpose(data3['scaledCv']['mean'][::6,:]), vmin = 0, vmax = 1)
     plt.show()
 
     ### profiles
     plt.plot(np.nanmean(obs['inversions']['scaledCv']['mean'],0),obs['inversions']['scaledZ'], color = 'k', linewidth = 2, label = 'Obs')
     plt.plot(np.nanmean(data1['scaledCv']['mean'][::6,:],0),data1['scaledZ'], color = 'steelblue', linewidth = 2, label = label1)
-    plt.plot(np.nanmean(data2['scaledCv']['mean'][::6,:],0),data2['scaledZ'], color = 'forestgreen', linewidth = 2, label = label1)
-    plt.plot(np.nanmean(data3['scaledCv']['mean'][::6,:],0),data3['scaledZ'], color = 'darkorange', linewidth = 2, label = label1)
+    plt.plot(np.nanmean(data2['scaledCv']['mean'][::6,:],0),data2['scaledZ'], color = 'forestgreen', linewidth = 2, label = label2)
+    plt.plot(np.nanmean(data3['scaledCv']['mean'][::6,:],0),data3['scaledZ'], color = 'darkorange', linewidth = 2, label = label3)
+    plt.legend()
     plt.show()
 
 def plot_LWP(um_data, ifs_data, misc_data, obs_data, month_flag, missing_files, um_out_dir, doy): #, lon, lat):
