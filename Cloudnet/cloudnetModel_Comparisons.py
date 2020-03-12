88a2d3b2e991eb5a46a316e0e582d40fdae080b2
gyoung410 <G.Young1@leeds.ac.uk>
2020-03-12 11:42:26 +0000

    change all "Cv" to "LWC"

--- Cloudnet/cloudnetModel_Comparisons.py
+++ Cloudnet/cloudnetModel_Comparisons.py
@@ -1175,23 +1175,23 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
     #### ---------------------------------------------------------------
 
     #### define empty arrays of nans to fill with scaled data
-    data1['scaledCv'] = {}
-    data1['scaledCv']['binned'] = {}
-    data1['scaledCv']['mean'] = np.zeros([np.size(um_data['height'],0),len(Zpts)]); data1['scaledCv']['mean'][:] = np.nan
-    data1['scaledCv']['stdev'] = np.zeros([np.size(um_data['height'],0),len(Zpts)]); data1['scaledCv']['stdev'][:] = np.nan
-    data1['blCv'] = np.zeros([np.size(um_data['height'],0),np.size(um_data['height'],1)]); data1['blCv'][:] = np.nan
+    data1['scaledLWC'] = {}
+    data1['scaledLWC']['binned'] = {}
+    data1['scaledLWC']['mean'] = np.zeros([np.size(um_data['height'],0),len(Zpts)]); data1['scaledLWC']['mean'][:] = np.nan
+    data1['scaledLWC']['stdev'] = np.zeros([np.size(um_data['height'],0),len(Zpts)]); data1['scaledLWC']['stdev'][:] = np.nan
+    data1['blLWC'] = np.zeros([np.size(um_data['height'],0),np.size(um_data['height'],1)]); data1['blLWC'][:] = np.nan
 
-    data2['scaledCv'] = {}
-    data2['scaledCv']['binned'] = {}
-    data2['scaledCv']['mean'] = np.zeros([np.size(misc_data['height'],0),len(Zpts)]); data2['scaledCv']['mean'][:] = np.nan
-    data2['scaledCv']['stdev'] = np.zeros([np.size(misc_data['height'],0),len(Zpts)]); data2['scaledCv']['stdev'][:] = np.nan
-    data2['blCv'] = np.zeros([np.size(misc_data['height'],0),np.size(misc_data['height'],1)]); data2['blCv'][:] = np.nan
+    data2['scaledLWC'] = {}
+    data2['scaledLWC']['binned'] = {}
+    data2['scaledLWC']['mean'] = np.zeros([np.size(misc_data['height'],0),len(Zpts)]); data2['scaledLWC']['mean'][:] = np.nan
+    data2['scaledLWC']['stdev'] = np.zeros([np.size(misc_data['height'],0),len(Zpts)]); data2['scaledLWC']['stdev'][:] = np.nan
+    data2['blLWC'] = np.zeros([np.size(misc_data['height'],0),np.size(misc_data['height'],1)]); data2['blLWC'][:] = np.nan
 
-    data3['scaledCv'] = {}
-    data3['scaledCv']['binned'] = {}
-    data3['scaledCv']['mean'] = np.zeros([np.size(ifs_data['height'],0),len(Zpts)]); data3['scaledCv']['mean'][:] = np.nan
-    data3['scaledCv']['stdev'] = np.zeros([np.size(ifs_data['height'],0),len(Zpts)]); data3['scaledCv']['stdev'][:] = np.nan
-    data3['blCv'] = np.zeros([np.size(ifs_data['height'],0),np.size(ifs_data['height'],1)]); data3['blCv'][:] = np.nan
+    data3['scaledLWC'] = {}
+    data3['scaledLWC']['binned'] = {}
+    data3['scaledLWC']['mean'] = np.zeros([np.size(ifs_data['height'],0),len(Zpts)]); data3['scaledLWC']['mean'][:] = np.nan
+    data3['scaledLWC']['stdev'] = np.zeros([np.size(ifs_data['height'],0),len(Zpts)]); data3['scaledLWC']['stdev'][:] = np.nan
+    data3['blLWC'] = np.zeros([np.size(ifs_data['height'],0),np.size(ifs_data['height'],1)]); data3['blLWC'][:] = np.nan
 
     ### save new height and cloudnet time array into dictionary (latter to account for missing files)
     data1['scaledZ'] = Zpts
@@ -1204,12 +1204,12 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
     ### ------------------------------------------------------------------------------------------
     ### find cloudnet timesteps which match the inversion timesteps
     ### ------------------------------------------------------------------------------------------
-    data1['scaledCv']['inversion_Tindex'] = np.zeros(np.size(tim1)); data1['scaledCv']['inversion_Tindex'][:] = np.nan
-    data1['scaledCv']['inversionForCloudnet'] = np.zeros(np.size(tim1)); data1['scaledCv']['inversionForCloudnet'][:] = np.nan
-    data2['scaledCv']['inversion_Tindex'] = np.zeros(np.size(tim2)); data2['scaledCv']['inversion_Tindex'][:] = np.nan
-    data2['scaledCv']['inversionForCloudnet'] = np.zeros(np.size(tim2)); data2['scaledCv']['inversionForCloudnet'][:] = np.nan
-    data3['scaledCv']['inversion_Tindex'] = np.zeros(np.size(tim3)); data3['scaledCv']['inversion_Tindex'][:] = np.nan
-    data3['scaledCv']['inversionForCloudnet'] = np.zeros(np.size(tim3)); data3['scaledCv']['inversionForCloudnet'][:] = np.nan
+    data1['scaledLWC']['inversion_Tindex'] = np.zeros(np.size(tim1)); data1['scaledLWC']['inversion_Tindex'][:] = np.nan
+    data1['scaledLWC']['inversionForCloudnet'] = np.zeros(np.size(tim1)); data1['scaledLWC']['inversionForCloudnet'][:] = np.nan
+    data2['scaledLWC']['inversion_Tindex'] = np.zeros(np.size(tim2)); data2['scaledLWC']['inversion_Tindex'][:] = np.nan
+    data2['scaledLWC']['inversionForCloudnet'] = np.zeros(np.size(tim2)); data2['scaledLWC']['inversionForCloudnet'][:] = np.nan
+    data3['scaledLWC']['inversion_Tindex'] = np.zeros(np.size(tim3)); data3['scaledLWC']['inversion_Tindex'][:] = np.nan
+    data3['scaledLWC']['inversionForCloudnet'] = np.zeros(np.size(tim3)); data3['scaledLWC']['inversionForCloudnet'][:] = np.nan
 
     for i in range(0, len(tim1)):
         ## find the cloudnet time INDEX which matches the inversion timestep
@@ -1220,7 +1220,7 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
             continue
         elif np.size(np.where(np.round(um_data['time'].data,3) == np.round(tim1[i],3))) > 0:
             ### gives INDEX of CLOUDNET DATA corresponding to that timestep
-            data1['scaledCv']['inversion_Tindex'][i] = np.where(np.round(um_data['time'].data,3) == np.round(tim1[i],3))[0][0]
+            data1['scaledLWC']['inversion_Tindex'][i] = np.where(np.round(um_data['time'].data,3) == np.round(tim1[i],3))[0][0]
         if np.floor(tim2[i]) in missing_files:
             ### don't assign a cloudnet index for missing files
             ### instead, set inv1 to nan at these times
@@ -1228,7 +1228,7 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
             continue
         elif np.size(np.where(np.round(misc_data['time'].data,3) == np.round(tim2[i],3))) > 0:
             ### gives INDEX of CLOUDNET DATA corresponding to that timestep
-            data2['scaledCv']['inversion_Tindex'][i] = np.where(np.round(misc_data['time'].data,3) == np.round(tim2[i],3))[0][0]
+            data2['scaledLWC']['inversion_Tindex'][i] = np.where(np.round(misc_data['time'].data,3) == np.round(tim2[i],3))[0][0]
         if np.floor(tim3[i]) in missing_files:
             ### don't assign a cloudnet index for missing files
             ### instead, set inv1 to nan at these times
@@ -1236,17 +1236,17 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
             continue
         elif np.size(np.where(np.round(ifs_data['time'].data,3) == np.round(tim3[i],3))) > 0:
             ### gives INDEX of CLOUDNET DATA corresponding to that timestep
-            data3['scaledCv']['inversion_Tindex'][i] = np.where(np.round(ifs_data['time'].data,3) == np.round(tim3[i],3))[0][0]
+            data3['scaledLWC']['inversion_Tindex'][i] = np.where(np.round(ifs_data['time'].data,3) == np.round(tim3[i],3))[0][0]
 
 
         ### use inversion_Tindices to define new inversion height array on cloudnet timesteps for looping over
         ###         if inversion_Tindex is not NaN, use to index inv into new array (cninv)
-        if data1['scaledCv']['inversion_Tindex'][i] >= 0.0:
-            data1['scaledCv']['inversionForCloudnet'][int(data1['scaledCv']['inversion_Tindex'][i])] = inv1[i]
-        if data2['scaledCv']['inversion_Tindex'][i] >= 0.0:
-            data2['scaledCv']['inversionForCloudnet'][int(data2['scaledCv']['inversion_Tindex'][i])] = inv2[i]
-        if data3['scaledCv']['inversion_Tindex'][i] >= 0.0:
-            data3['scaledCv']['inversionForCloudnet'][int(data3['scaledCv']['inversion_Tindex'][i])] = inv3[i]
+        if data1['scaledLWC']['inversion_Tindex'][i] >= 0.0:
+            data1['scaledLWC']['inversionForCloudnet'][int(data1['scaledLWC']['inversion_Tindex'][i])] = inv1[i]
+        if data2['scaledLWC']['inversion_Tindex'][i] >= 0.0:
+            data2['scaledLWC']['inversionForCloudnet'][int(data2['scaledLWC']['inversion_Tindex'][i])] = inv2[i]
+        if data3['scaledLWC']['inversion_Tindex'][i] >= 0.0:
+            data3['scaledLWC']['inversionForCloudnet'][int(data3['scaledLWC']['inversion_Tindex'][i])] = inv3[i]
 
     np.save('working_data1', data1)
     np.save('working_data2', data2)
@@ -1266,12 +1266,12 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
     for i in range(0, np.size(um_data['time'])):        ### all can go in this loop, um_data['time'] == hourly data
 
         ### main inversion base assignments
-        if np.size(np.where(um_data['height'][i,:].data == data1['scaledCv']['inversionForCloudnet'][i])) > 0.0:
-            zind1[i] = np.where(um_data['height'][i,:].data == data1['scaledCv']['inversionForCloudnet'][i])[0][0]
-        if np.size(np.where(data2['height'][1:].data == data2['scaledCv']['inversionForCloudnet'][i])) > 0.0:
-            zind2[i] = np.where(data2['height'][1:].data == data2['scaledCv']['inversionForCloudnet'][i])[0][0]
-        if np.size(np.where(data3['height_hrly'][i].data <= data3['scaledCv']['inversionForCloudnet'][i])) > 0.0:
-            temp = data3['height_hrly'][i,:].data <= data3['scaledCv']['inversionForCloudnet'][i]
+        if np.size(np.where(um_data['height'][i,:].data == data1['scaledLWC']['inversionForCloudnet'][i])) > 0.0:
+            zind1[i] = np.where(um_data['height'][i,:].data == data1['scaledLWC']['inversionForCloudnet'][i])[0][0]
+        if np.size(np.where(data2['height'][1:].data == data2['scaledLWC']['inversionForCloudnet'][i])) > 0.0:
+            zind2[i] = np.where(data2['height'][1:].data == data2['scaledLWC']['inversionForCloudnet'][i])[0][0]
+        if np.size(np.where(data3['height_hrly'][i].data <= data3['scaledLWC']['inversionForCloudnet'][i])) > 0.0:
+            temp = data3['height_hrly'][i,:].data <= data3['scaledLWC']['inversionForCloudnet'][i]
             zind3[i] = np.where(temp == True)[0][-1]
 
     #### assign height indices to dictionary for later use
@@ -1317,9 +1317,9 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
         print(str(i) + 'th timestep (model data):')
 
         ### create new dictionary entry for i-th timestep
-        data1['scaledCv']['binned']['t' + str(i)] = {}
-        data2['scaledCv']['binned']['t' + str(i)] = {}
-        data3['scaledCv']['binned']['t' + str(i)] = {}
+        data1['scaledLWC']['binned']['t' + str(i)] = {}
+        data2['scaledLWC']['binned']['t' + str(i)] = {}
+        data3['scaledLWC']['binned']['t' + str(i)] = {}
 
         ###-----------------------------------------------------------------------------------------
         ### for main inversion
@@ -1344,9 +1344,9 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
         scaled_hgts3 = hgts3 / ifs_data['height'][i,int(data3['inversions']['invbase_kIndex'][i])]
 
         # find Cv values below the BL inversion
-        data1['blCv'][i,:int(data1['inversions']['invbase_kIndex'][i])] = um_data['model_Cv_filtered'][i,:int(data1['inversions']['invbase_kIndex'][i])]
-        data2['blCv'][i,:int(data2['inversions']['invbase_kIndex'][i])] = misc_data['model_Cv_filtered'][i,:int(data2['inversions']['invbase_kIndex'][i])]
-        data3['blCv'][i,:int(data3['inversions']['invbase_kIndex'][i])] = ifs_data['model_snow_Cv_filtered'][i,:int(data3['inversions']['invbase_kIndex'][i])]
+        data1['blLWC'][i,:int(data1['inversions']['invbase_kIndex'][i])] = um_data['model_lwc'][i,:int(data1['inversions']['invbase_kIndex'][i])]
+        data2['blLWC'][i,:int(data2['inversions']['invbase_kIndex'][i])] = misc_data['model_lwc'][i,:int(data2['inversions']['invbase_kIndex'][i])]
+        data3['blLWC'][i,:int(data3['inversions']['invbase_kIndex'][i])] = ifs_data['model_lwc'][i,:int(data3['inversions']['invbase_kIndex'][i])]
 
         ## bin scaled BL heights into pre-set Zpts array so every timestep can be compared
         for k in range(len(Zpts)):
@@ -1354,20 +1354,20 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
             tempvar2 = np.where(np.logical_and(scaled_hgts2 >= Zpts[k] - binres/2.0, scaled_hgts2 < Zpts[k] + binres/2.0))
             tempvar3 = np.where(np.logical_and(scaled_hgts3 >= Zpts[k] - binres/2.0, scaled_hgts3 < Zpts[k] + binres/2.0))
 
-            data1['scaledCv']['binned']['t' + str(i)][Zpts[k]] = data1['blCv'][i,tempvar1]
-            if np.size(data1['scaledCv']['binned']['t' + str(i)][Zpts[k]]) > 0:
-                data1['scaledCv']['mean'][i,k] = np.nanmean(data1['scaledCv']['binned']['t' + str(i)][Zpts[k]])
-            data1['scaledCv']['stdev'][i,k] = np.nanstd(data1['scaledCv']['binned']['t' + str(i)][Zpts[k]])
+            data1['scaledLWC']['binned']['t' + str(i)][Zpts[k]] = data1['blLWC'][i,tempvar1]
+            if np.size(data1['scaledLWC']['binned']['t' + str(i)][Zpts[k]]) > 0:
+                data1['scaledLWC']['mean'][i,k] = np.nanmean(data1['scaledLWC']['binned']['t' + str(i)][Zpts[k]])
+            data1['scaledLWC']['stdev'][i,k] = np.nanstd(data1['scaledLWC']['binned']['t' + str(i)][Zpts[k]])
 
-            data2['scaledCv']['binned']['t' + str(i)][Zpts[k]] = data2['blCv'][i,tempvar2]
-            if np.size(data2['scaledCv']['binned']['t' + str(i)][Zpts[k]]) > 0:
-                data2['scaledCv']['mean'][i,k] = np.nanmean(data2['scaledCv']['binned']['t' + str(i)][Zpts[k]])
-            data2['scaledCv']['stdev'][i,k] = np.nanstd(data2['scaledCv']['binned']['t' + str(i)][Zpts[k]])
+            data2['scaledLWC']['binned']['t' + str(i)][Zpts[k]] = data2['blLWC'][i,tempvar2]
+            if np.size(data2['scaledLWC']['binned']['t' + str(i)][Zpts[k]]) > 0:
+                data2['scaledLWC']['mean'][i,k] = np.nanmean(data2['scaledLWC']['binned']['t' + str(i)][Zpts[k]])
+            data2['scaledLWC']['stdev'][i,k] = np.nanstd(data2['scaledLWC']['binned']['t' + str(i)][Zpts[k]])
 
-            data3['scaledCv']['binned']['t' + str(i)][Zpts[k]] = data3['blCv'][i,tempvar3]
-            if np.size(data3['scaledCv']['binned']['t' + str(i)][Zpts[k]]) > 0:
-                data3['scaledCv']['mean'][i,k] = np.nanmean(data3['scaledCv']['binned']['t' + str(i)][Zpts[k]])
-            data3['scaledCv']['stdev'][i,k] = np.nanstd(data3['scaledCv']['binned']['t' + str(i)][Zpts[k]])
+            data3['scaledLWC']['binned']['t' + str(i)][Zpts[k]] = data3['blLWC'][i,tempvar3]
+            if np.size(data3['scaledLWC']['binned']['t' + str(i)][Zpts[k]]) > 0:
+                data3['scaledLWC']['mean'][i,k] = np.nanmean(data3['scaledLWC']['binned']['t' + str(i)][Zpts[k]])
+            data3['scaledLWC']['stdev'][i,k] = np.nanstd(data3['scaledLWC']['binned']['t' + str(i)][Zpts[k]])
 
     ##################################################
     ##################################################
@@ -1378,60 +1378,60 @@ def plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data
     ### timeseries
     plt.subplot(411)
     plt.title('Obs')
-    plt.pcolor(obs['inversions']['scaledTime'],obs['inversions']['scaledZ'],np.transpose(obs['inversions']['scaledCv']['mean']), vmin = 0, vmax = 1)
+    plt.pcolor(obs['inversions']['scaledTime'],obs['inversions']['scaledZ'],np.transpose(obs['inversions']['scaledLWC']['mean']), vmin = 0, vmax = 1)
     plt.subplot(412)
     plt.title(label1)
-    plt.pcolor(data1['scaledTime'],data1['scaledZ'],np.transpose(data1['scaledCv']['mean']), vmin = 0, vmax = 1)
+    plt.pcolor(data1['scaledTime'],data1['scaledZ'],np.transpose(data1['scaledLWC']['mean']), vmin = 0, vmax = 1)
     plt.subplot(413)
     plt.title(label2)
-    plt.pcolor(data2['scaledTime'],data2['scaledZ'],np.transpose(data2['scaledCv']['mean']), vmin = 0, vmax = 1)
+    plt.pcolor(data2['scaledTime'],data2['scaledZ'],np.transpose(data2['scaledLWC']['mean']), vmin = 0, vmax = 1)
     plt.subplot(414)
     plt.title(label3)
-    plt.pcolor(data3['scaledTime'],data3['scaledZ'],np.transpose(data3['scaledCv']['mean']), vmin = 0, vmax = 1)
+    plt.pcolor(data3['scaledTime'],data3['scaledZ'],np.transpose(data3['scaledLWC']['mean']), vmin = 0, vmax = 1)
     plt.show()
 
     ### obs
     plt.subplot(211)
-    plt.pcolor(obs_data['time_6hrly'].data,obs_data['height_6hrly'][0,:].data,np.transpose(obs['inversions']['blCv'])); plt.ylim([0,3e3])
+    plt.pcolor(obs_data['time_6hrly'].data,obs_data['height_6hrly'][0,:].data,np.transpose(obs['inversions']['blLWC'])); plt.ylim([0,3e3])
     plt.plot(np.squeeze(obs['inversions']['doy_drift']),np.squeeze(obs['inversions']['invbase'][drift]),'r')
     plt.xlim([226,258])
     plt.subplot(212)
-    plt.pcolor(obs['inversions']['scaledTime'],obs['inversions']['scaledZ'],np.transpose(obs['inversions']['scaledCv']['mean'])); plt.ylim([0,1])
+    plt.pcolor(obs['inversions']['scaledTime'],obs['inversions']['scaledZ'],np.transpose(obs['inversions']['scaledLWC']['mean'])); plt.ylim([0,1])
     plt.xlim([226,258])
     plt.show()
 
     ### um_ra2m
     plt.subplot(211)
-    plt.pcolor(um_data['time'].data,um_data['height'][0,:].data,np.transpose(data1['blCv'])); plt.ylim([0,3e3])
+    plt.pcolor(um_data['time'].data,um_data['height'][0,:].data,np.transpose(data1['blLWC'])); plt.ylim([0,3e3])
     plt.plot(data1['inversions']['doy_drift'],np.squeeze(data1['inversions']['invbase_drift']),'r')
     plt.xlim([226,258])
     plt.subplot(212)
-    plt.pcolor(data1['scaledTime'],data1['scaledZ'],np.transpose(data1['scaledCv']['mean'])); plt.ylim([0,1])
+    plt.pcolor(data1['scaledTime'],data1['scaledZ'],np.transpose(data1['scaledLWC']['mean'])); plt.ylim([0,1])
     plt.xlim([226,258])
     plt.show()
 
     ### um_casim-100
     plt.subplot(211)
-    plt.pcolor(misc_data['time'].data,misc_data['height'][0,:].data,np.transpose(data2['blCv'])); plt.ylim([0,3e3])
+    plt.pcolor(misc_data['time'].data,misc_data['height'][0,:].data,np.transpose(data2['blLWC'])); plt.ylim([0,3e3])
     plt.plot(data2['inversions']['doy'],np.squeeze(data2['inversions']['invbase']),'r')
     plt.xlim([226,258])
     plt.subplot(212)
-    plt.pcolor(data2['scaledTime'],data2['scaledZ'],np.transpose(data2['scaledCv']['mean'])); plt.ylim([0,1])
+    plt.pcolor(data2['scaledTime'],data2['scaledZ'],np.transpose(data2['scaledLWC']['mean'])); plt.ylim([0,1])
     plt.xlim([226,258])
     plt.show()
 
     ### ecmwf_ifs
     plt.subplot(211)
-    plt.pcolor(ifs_data['time'].data,ifs_data['height'][0,:].data,np.transpose(data3['blCv'])); plt.ylim([0,3e3])
+    plt.pcolor(ifs_data['time'].data,ifs_data['height'][0,:].data,np.transpose(data3['blLWC'])); plt.ylim([0,3e3])
     plt.plot(data3['inversions']['doy'],np.squeeze(data3['inversions']['invbase']),'r')
     plt.xlim([226,258])
     plt.subplot(212)
-    plt.pcolor(data3['scaledTime'],data3['scaledZ'],np.transpose(data3['scaledCv']['mean'])); plt.ylim([0,1])
+    plt.pcolor(data3['scaledTime'],data3['scaledZ'],np.transpose(data3['scaledLWC']['mean'])); plt.ylim([0,1])
     plt.xlim([226,258])
     plt.show()
 
     ### profiles
-    plt.plot(np.nanmean(obs['inversions']['scaledCv']['mean'],0),obs['inversions']['scaledZ'], color = 'k', linewidth = 2, label = 'Obs')
+    plt.plot(np.nanmean(obs['inversions']['scaledLWC']['mean'],0),obs['inversions']['scaledZ'], color = 'k', linewidth = 2, label = 'Obs')
     plt.plot(np.nanmean(data1['scaledCv']['mean'][::6,:],0),data1['scaledZ'], color = 'steelblue', linewidth = 2, label = label1)
     plt.plot(np.nanmean(data2['scaledCv']['mean'][::6,:],0),data2['scaledZ'], color = 'forestgreen', linewidth = 2, label = label2)
     plt.plot(np.nanmean(data3['scaledCv']['mean'][::6,:],0),data3['scaledZ'], color = 'darkorange', linewidth = 2, label = label3)
