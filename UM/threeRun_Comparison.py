458ff55b7e2b20c1966f9729eeba3321c741511e
gyoung410 <G.Young1@leeds.ac.uk>
2020-03-25 11:22:58 +0000

    time -> height

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -3563,7 +3563,7 @@ def write_reGrid(data1, data2, data3, obs, varlist):
     ## Data dimensions
     ####################################
     times = nc1.createDimension('time', np.size(data1['time_6hrly']))
-    height = nc1.createDimension('time', np.size(data1['universal_height']))
+    height = nc1.createDimension('height', np.size(data1['universal_height']))
 
     ###################################
     ## Dimensions variables
