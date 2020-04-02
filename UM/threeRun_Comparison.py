137cbfd17c2c3d723b0370f5ad58a2608b7e88da
gyoung410 <G.Young1@leeds.ac.uk>
2020-04-02 17:58:35 +0100

    check obs twice too

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -3968,6 +3968,8 @@ def inversionIdent(data1, data2, data3, month_flag, missing_files, out_dir1, out
         #### ---------------------------------------------------------------
         #### check if strong gradient starts at i-2
         #### ---------------------------------------------------------------
+        if obs['sondes']['thetaE_Diff'][i,int(obs['sondes']['thetaE_invbaseID'][i])-1] > thresh:
+            obs['sondes']['thetaE_invbaseID'][i] = int(obs['sondes']['thetaE_invbaseID'][i]) - 1
         if data1['thetaE_6hrlyDiff'][i,int(data1['thetaE_invbaseID'][i])-1] > thresh:
             data1['thetaE_invbaseID'][i] = int(data1['thetaE_invbaseID'][i]) - 1
         if data2['thetaE_6hrlyDiff'][i,int(data2['thetaE_invbaseID'][i])-1] > thresh:
