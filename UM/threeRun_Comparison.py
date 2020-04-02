a489f875bd6e5adae0474b1cd88dcaa18c249969
gyoung410 <G.Young1@leeds.ac.uk>
2020-04-02 17:30:35 +0100

    add max condition to ignore ifs missing files

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -3964,7 +3964,8 @@ def inversionIdent(data1, data2, data3, month_flag, missing_files, out_dir1, out
         # obs['sondes']['thetaE_invbaseJVID'][i] = np.where(data1['universal_height'] == obs['inversions']['invbase'][i])
         data1['thetaE_invbaseJVID'][i] = np.where(data1['universal_height'] == data1['inversions']['invbase'][i])[0][0]
         data2['thetaE_invbaseJVID'][i] = np.where(data1['universal_height'] == data2['inversions']['invbase'][i])[0][0]
-        data3['thetaE_invbaseJVID'][i] = np.where(data1['universal_height'] == data3['inversions']['invbase'][i])[0][0]
+        if np.nanmax(data3['thetaE_6hrlyDiff'][i,lt3000]) >= 0.0:
+            data3['thetaE_invbaseJVID'][i] = np.where(data1['universal_height'] == data3['inversions']['invbase'][i])[0][0]
 
     #### ---------------------------------------------------------------
     #### save quicklooks for reference
