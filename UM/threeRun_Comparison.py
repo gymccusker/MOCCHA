664dc768b6bab662c7cb7ae3820fbc670488d13d
gyoung410 <G.Young1@leeds.ac.uk>
2020-04-03 13:15:57 +0100

    should be 2ndinvID, not -1!

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -4003,14 +4003,14 @@ def inversionIdent(data1, data2, data3, month_flag, missing_files, out_dir1, out
         if np.nanmax(data3['thetaE_6hrlyDiff'][i,lt3000]) >= 0.0:
             data3['thetaE_2ndinvID'][i] = np.where(data3['thetaE_6hrlyDiff'][i,:] == np.sort(data3['thetaE_6hrlyDiff'][i,:int(data3['thetaE_invbaseID'][i])+1])[::-1][1])[0][0]
         ### 3. check if second strongest inversion is greater than 2K: if so, label as top of decoupled stable layer
-        if obs['sondes']['thetaE_Diff'][i,int(obs['sondes']['thetaE_2ndinvID'][i])-1] < thresh:
+        if obs['sondes']['thetaE_Diff'][i,int(obs['sondes']['thetaE_2ndinvID'][i])] < thresh:
             obs['sondes']['thetaE_2ndinvID'][i] = 0
-        if data1['thetaE_6hrlyDiff'][i,int(data1['thetaE_2ndinvID'][i])-1] < thresh:
+        if data1['thetaE_6hrlyDiff'][i,int(data1['thetaE_2ndinvID'][i])] < thresh:
             data1['thetaE_2ndinvID'][i] = 0
-        if data2['thetaE_6hrlyDiff'][i,int(data2['thetaE_2ndinvID'][i])-1] < thresh:
+        if data2['thetaE_6hrlyDiff'][i,int(data2['thetaE_2ndinvID'][i])] < thresh:
             data2['thetaE_2ndinvID'][i] = 0
         if np.nanmax(data3['thetaE_6hrlyDiff'][i,lt3000]) >= 0.0:
-            if data3['thetaE_6hrlyDiff'][i,int(data3['thetaE_2ndinvID'][i])-1] < thresh:
+            if data3['thetaE_6hrlyDiff'][i,int(data3['thetaE_2ndinvID'][i])] < thresh:
                 data3['thetaE_2ndinvID'][i] = 0
 
     #### ---------------------------------------------------------------
