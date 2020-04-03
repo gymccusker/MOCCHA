3b86c8ed036f37aaf2949547d6d20a7b1ac5209b
gyoung410 <G.Young1@leeds.ac.uk>
2020-04-03 10:58:22 +0100

    can't set array with sequence

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -3992,10 +3992,10 @@ def inversionIdent(data1, data2, data3, month_flag, missing_files, out_dir1, out
         data2['thetaE_orderedInv'] = np.sort(data2['thetaE_6hrlyDiff'][:,lt3000[::-1]])
         data3['thetaE_orderedInv'] = np.sort(data3['thetaE_6hrlyDiff'][:,lt3000[::-1]])
         ### 2. check for second strongest inversion below invbaseID
-        obs['sondes']['thetaE_2ndinvID'][i] = np.where(obs['sondes']['thetaE_Diff'][i,:] == np.sort(obs['sondes']['thetaE_Diff'][i,:int(obs['sondes']['thetaE_invbaseID'][0])+1])[::-1][1])
-        data1['thetaE_2ndinvID'][i] = np.where(data1['thetaE_6hrlyDiff'][i,:] == np.sort(data1['thetaE_6hrlyDiff'][i,:int(data1['thetaE_invbaseID'][0])+1])[::-1][1])
-        data2['thetaE_2ndinvID'][i] = np.where(data2['thetaE_6hrlyDiff'][i,:] == np.sort(data2['thetaE_6hrlyDiff'][i,:int(data2['thetaE_invbaseID'][0])+1])[::-1][1])
-        data3['thetaE_2ndinvID'][i] = np.where(data3['thetaE_6hrlyDiff'][i,:] == np.sort(data3['thetaE_6hrlyDiff'][i,:int(data3['thetaE_invbaseID'][0])+1])[::-1][1])
+        obs['sondes']['thetaE_2ndinvID'][i] = np.where(obs['sondes']['thetaE_Diff'][i,:] == np.sort(obs['sondes']['thetaE_Diff'][i,:int(obs['sondes']['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
+        data1['thetaE_2ndinvID'][i] = np.where(data1['thetaE_6hrlyDiff'][i,:] == np.sort(data1['thetaE_6hrlyDiff'][i,:int(data1['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
+        data2['thetaE_2ndinvID'][i] = np.where(data2['thetaE_6hrlyDiff'][i,:] == np.sort(data2['thetaE_6hrlyDiff'][i,:int(data2['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
+        data3['thetaE_2ndinvID'][i] = np.where(data3['thetaE_6hrlyDiff'][i,:] == np.sort(data3['thetaE_6hrlyDiff'][i,:int(data3['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
 
     #### ---------------------------------------------------------------
     #### save quicklooks for reference
