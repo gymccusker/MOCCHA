406b0c2015d0e2ec5cff8ad59afe285448f3d35d
gyoung410 <G.Young1@leeds.ac.uk>
2020-04-03 10:58:54 +0100

    need if statement to account for missing files with ifs

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -3995,7 +3995,8 @@ def inversionIdent(data1, data2, data3, month_flag, missing_files, out_dir1, out
         obs['sondes']['thetaE_2ndinvID'][i] = np.where(obs['sondes']['thetaE_Diff'][i,:] == np.sort(obs['sondes']['thetaE_Diff'][i,:int(obs['sondes']['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
         data1['thetaE_2ndinvID'][i] = np.where(data1['thetaE_6hrlyDiff'][i,:] == np.sort(data1['thetaE_6hrlyDiff'][i,:int(data1['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
         data2['thetaE_2ndinvID'][i] = np.where(data2['thetaE_6hrlyDiff'][i,:] == np.sort(data2['thetaE_6hrlyDiff'][i,:int(data2['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
-        data3['thetaE_2ndinvID'][i] = np.where(data3['thetaE_6hrlyDiff'][i,:] == np.sort(data3['thetaE_6hrlyDiff'][i,:int(data3['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
+        if np.nanmax(data3['thetaE_6hrlyDiff'][i,lt3000]) >= 0.0:
+            data3['thetaE_2ndinvID'][i] = np.where(data3['thetaE_6hrlyDiff'][i,:] == np.sort(data3['thetaE_6hrlyDiff'][i,:int(data3['thetaE_invbaseID'][0])+1])[::-1][1])[0][0]
 
     #### ---------------------------------------------------------------
     #### save quicklooks for reference
