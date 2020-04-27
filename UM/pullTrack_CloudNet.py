1bf376e4f31943afe188d504394244f3bc47636a
gyoung410 <G.Young1@leeds.ac.uk>
2020-04-27 15:54:53 +0100

    pull latest tracks for 12_u-br210_RA1M_CASIM/

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -7507,7 +7507,7 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '13_u-br409_RA1M_CASIM/'
+    out_dir = '12_u-br210_RA1M_CASIM/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 4_u-bg610_RA2M_CON/              # Wilson and Ballard 1999 uphys
