48e81697896e6a0532e30f349ed65c4d138c9432
gyoung410 <G.Young1@leeds.ac.uk>
2020-04-29 16:10:12 +0100

    ignore 20180824, 20180827, and 20180828 as still waiting for those dates

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -4892,8 +4892,8 @@ def main():
 
     moccha_names = [#'20180814_oden_','20180815_oden_','20180816_oden_',
             '20180817_oden_','20180818_oden_','20180819_oden_','20180820_oden_',
-            '20180821_oden_','20180822_oden_','20180823_oden_','20180824_oden_',
-            '20180825_oden_','20180826_oden_','20180827_oden_','20180828_oden_',
+            '20180821_oden_','20180822_oden_','20180823_oden_',
+            '20180825_oden_','20180826_oden_',
             '20180829_oden_','20180830_oden_','20180831_oden_','20180901_oden_',
             '20180902_oden_','20180903_oden_','20180904_oden_','20180905_oden_',
             '20180906_oden_','20180907_oden_','20180908_oden_','20180909_oden_',
@@ -4903,7 +4903,7 @@ def main():
 
     Sep_missing_files = []
 
-    moccha_missing_files = ['20180813_oden_']
+    moccha_missing_files = ['20180813_oden_','20180824_oden_','20180827_oden_','20180828_oden_',]
 
     # doy = np.arange(226,259)        ## set DOY for full drift figures (over which we have cloudnet data)
     # doy = np.arange(240,251)        ## set DOY for subset of drift figures (presentations)
