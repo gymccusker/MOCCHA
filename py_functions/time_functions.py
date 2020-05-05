088b12074c6110bdb0f0cd59bcaa483b1ed47556
gyoung410 <G.Young1@leeds.ac.uk>
2020-05-05 11:13:26 +0100

    remove print statements for py2 and py3 compatibility

--- py_functions/time_functions.py
+++ py_functions/time_functions.py
@@ -14,7 +14,7 @@ def calcTime_Mat2DOY(matlab_time):
         #### EXAMPLE OF USE:
         #### pytime = calcTime_Mat2DOY(matlab_time)
 
-    print 'Converting MATLAB timesteps to DOY:'
+    # print 'Converting MATLAB timesteps to DOY:'
 
     timestamps = pd.to_datetime(matlab_time-719529, unit='D')
     python_time = timestamps.dayofyear + (timestamps.hour / 24.0) + (timestamps.minute / 1440.0) + (timestamps.second / 86400.0)
@@ -25,7 +25,7 @@ def calcTime_Date2DOY(date):
 
     ####        date should be forematted as YYYYmmDD
 
-    print 'Converting date to DOY:'
+    # print 'Converting date to DOY:'
 
     mm = date[4:6]      #### month
     DD = date[6:8]      #### day
@@ -37,11 +37,11 @@ def calcTime_Date2DOY(date):
     elif mm == '09':
         doy = float(DD) + refDateSep
     else:
-        print '****Date not valid with this function****'
+        # print '****Date not valid with this function****'
 
-    print '----'
-    print 'Date = ', date
-    print 'DOY = ', doy
-    print ''
+    # print '----'
+    # print 'Date = ', date
+    # print 'DOY = ', doy
+    # print ''
 
     return doy
