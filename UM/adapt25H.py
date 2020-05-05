cd7479fcff28691dcf4291692ca9cffaf16f04a7
gyoung410 <G.Young1@leeds.ac.uk>
2020-05-05 11:10:29 +0100

    start at 20180814

--- UM/adapt25H.py
+++ UM/adapt25H.py
@@ -403,7 +403,7 @@ def main():
             '20180909_oden_metum.nc','20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc',
             '20180913_oden_metum.nc','20180914_oden_metum.nc']
 
-    moccha_names = ['20180813_oden_metum.nc','20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
+    moccha_names = ['20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
             '20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
             '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
             '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
@@ -413,7 +413,7 @@ def main():
             '20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc',
             '20180914_oden_metum.nc']
 
-    Aug_missing_files = ['20180812_oden_metum.nc']
+    Aug_missing_files = ['20180812_oden_metum.nc','20180813_oden_metum.nc']
 
     Sep_missing_files = []
 
@@ -422,7 +422,7 @@ def main():
     # doy = np.arange(225,258)        ## set DOY for full moccha figures
     # doy = np.arange(243,259)        ## set DOY for subset of moccha figures
     # doy = np.arange(240,251)        ## set DOY for subset of moccha figures
-    doy = np.arange(225,259)        ## set DOY for CASIM-AeroProf (17th Aug to 14th Sep)
+    doy = np.arange(226,259)        ## set DOY for CASIM-AeroProf (17th Aug to 14th Sep)
 
     ## Choose month:
     names = moccha_names
