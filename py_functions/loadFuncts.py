46551173b05301885035ab9d70703857f41fded5
gyoung410 <G.Young1@leeds.ac.uk>
2020-06-03 13:15:32 +0100

    initial commit with bare-bones load_radar funct

---
+++ py_functions/loadFuncts.py
@@ -0,0 +1,18 @@
+"""
+Functions for loading specific datasets
+==============================
+
+"""
+
+import numpy as np
+# from __future__ import print_function
+
+def load_radar(data1, data2, data3, obs, doy, var):
+
+    """
+    Function to load in radar data
+    """
+
+
+
+    return
