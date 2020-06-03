6f76eb1efda9fbbaa29f840179aad2ee891e4301
gyoung410 <G.Young1@leeds.ac.uk>
2020-06-03 13:43:01 +0100

    add filename referencing day input

--- py_functions/loadFuncts.py
+++ py_functions/loadFuncts.py
@@ -7,12 +7,17 @@ Functions for loading specific datasets
 import numpy as np
 # from __future__ import print_function
 
-def load_radar(data1, data2, data3, obs, doy, var):
+def load_radar(day, proj):
 
     """
     Function to load in radar data
+    Set up to take 'proj' argument, options:
+        1. moccha
     """
 
+    if proj == 'moccha':
+        data_dir = '/home/gillian/MOCCHA/ODEN/DATA/'
+        file_name = data_dir + 'mmcr/' day + '_Oden_mira.nc'];
 
 
     return
