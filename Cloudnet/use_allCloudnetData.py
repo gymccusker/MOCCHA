e869522453985d845f435700392bb448100cc652
gyoung410 <G.Young1@leeds.ac.uk>
2020-06-04 11:07:19 +0100

    put height bypass in front of ndim=1 if statement

--- Cloudnet/use_allCloudnetData.py
+++ Cloudnet/use_allCloudnetData.py
@@ -4004,10 +4004,10 @@ def main():
                     for c in range(0,3):
                         for j in range(0,len(obs_var_list[c])):
                             # print 'j = ' + str(j)
-                            if np.ndim(cn_nc0[c].variables[obs_var_list[c][j]]) == 1:
-                                obs_data[obs_var_list[c][j]] = np.append(obs_data[obs_var_list[c][j]],cn_nc0[c].variables[obs_var_list[c][j]][:])
-                            elif obs_var_list[c][j] == 'height':
+                            if obs_var_list[c][j] == 'height':
                                 continue
+                            elif np.ndim(cn_nc0[c].variables[obs_var_list[c][j]]) == 1:
+                                obs_data[obs_var_list[c][j]] = np.append(obs_data[obs_var_list[c][j]],cn_nc0[c].variables[obs_var_list[c][j]][:])
                             elif np.ndim(cn_nc0[c].variables[obs_var_list[c][j]]) == 2:
                                 obs_data[obs_var_list[c][j]] = np.append(obs_data[obs_var_list[c][j]],cn_nc0[c].variables[obs_var_list[c][j]][:],0)
                 for c in range(0,3): cn_nc0[c].close()
