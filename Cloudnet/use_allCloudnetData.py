6dbf74e5b3212180a0eb98bd41f807ae722dfa19
gyoung410 <G.Young1@leeds.ac.uk>
2020-06-12 10:09:56 +0100

    plot ecmwf-grid obs data

--- Cloudnet/use_allCloudnetData.py
+++ Cloudnet/use_allCloudnetData.py
@@ -3497,7 +3497,7 @@ def main():
                         'lwc-scaled-metum-grid/2018/',
                         'iwc-Z-T-metum-grid/2018/']
         elif obs_switch == 'RADAR':
-            cn_obs_out_dir = ['cloud-fraction-metum-grid/2018/',
+            cn_obs_out_dir = ['cloud-fraction-ecmwf-grid/2018/',
                         'lwc-scaled-adiabatic/2018/',
                         'iwc-Z-T-method/2018/']
         if cn_misc_flag == 0:       ## flag to compare cloudnet model data
