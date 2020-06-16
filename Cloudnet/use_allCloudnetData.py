1935f287a75c501dadc9bc8034eb50075e9a0775
gyoung410 <G.Young1@leeds.ac.uk>
2020-06-16 10:41:39 +0100

    should be cn_um_out_dir

--- Cloudnet/use_allCloudnetData.py
+++ Cloudnet/use_allCloudnetData.py
@@ -4343,7 +4343,7 @@ def main():
     # Cloudnet plot: Plot Cv statistics from drift period
     # -------------------------------------------------------------
     figure = plot_CvProfiles(um_data, ifs_data, misc_data, obs_data, month_flag, missing_files, cn_um_out_dir, doy, obs, obs_switch)
-    figure = plot_lwcProfiles(um_data, ifs_data, misc_data, obs_data, month_flag, missing_files, um_out_dir, doy, obs_switch)
+    figure = plot_lwcProfiles(um_data, ifs_data, misc_data, obs_data, month_flag, missing_files, cn_um_out_dir, doy, obs_switch)
 
     # -------------------------------------------------------------
     # Cloudnet plot: Plot contour timeseries
