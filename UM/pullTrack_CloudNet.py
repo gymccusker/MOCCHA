f4b0066ed4a99328c462567f21fa486d51338e26
gyoung410 <G.Young1@leeds.ac.uk>
2020-06-26 12:12:58 +0100

    need to loop over lons separately since lats/lons different size

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -5536,13 +5536,16 @@ def readGlobal(cube, ship_data, date_dir):
         print 'hour = ' + str(h)
         for j in range(0,len(sb_lats)):     ### for all latitude points
             if np.logical_and(ship_lats[h] >= sb_lats[j], ship_lats[h] < nb_lats[j]):     ### find where ship lat sits on glm lat grid
-                if np.logical_and(ship_lons[h] >= wb_lons[j], ship_lons[h] < eb_lons[j]):     ### find where ship lon sits on glm lon grid
-                    print 'lats and lons match at j = ' + str(j)
-                    jflag[h] = jflag[h] + 1
-                    data['ship_lons'][h] = lons[j]
-                    data['ship_hour'][h] = hours[h]
-                    data['ship_lats'][h] = lats[j]
-                    data['ship_ind'][h] = j         # define grid point indices for use later
+                for i in range(0,len(wb_lons)):     ### for all longitude points
+                    if np.logical_and(ship_lons[h] >= wb_lons[i], ship_lons[h] < eb_lons[i]):     ### find where ship lon sits on glm lon grid
+                        print 'lats and lons match at j = ' + str(j)
+                        jflag[h] = jflag[h] + 1
+                        data['ship_lons'][h] = lons[i]
+                        data['ship_hour'][h] = hours[h]
+                        data['ship_lats'][h] = lats[j]
+                        data['ship_ind'][h] = j         # define grid point indices for use later
+
+    print data['ship']
 
     #####--------------------------------------------------------------------------------------------------
     #####--------------------------------------------------------------------------------------------------
