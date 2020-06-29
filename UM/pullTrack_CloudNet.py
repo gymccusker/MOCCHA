46dede3265640d162707bcb1a28d18cc7926e0ba
gyoung410 <G.Young1@leeds.ac.uk>
2020-06-29 11:04:51 +0100

    run for all available dates in u-bn068

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -7748,11 +7748,11 @@ def main():
 
     for date in date_dir:
         ### just do 2018 dates
-        # if date[0:4] == '2018':
+        if date[0:4] == '2018':
         ### just do first date:
         # if date == date_dir[0]:
         ### just do specific date
-        if date[0:8] == '20180827':
+        # if date[0:8] == '20180827':
             # # -------------------------------------------------------------
             # # Load cube
             # # -------------------------------------------------------------
