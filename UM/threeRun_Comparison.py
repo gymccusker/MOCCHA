a23ebb7dfe35d9885718094a6645cf997b1bdda9
gyoung410 <G.Young1@leeds.ac.uk>
2020-07-02 16:53:00 +0100

    plot glm vs ifs comparison

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -5084,9 +5084,9 @@ def main():
     ### CHOSEN RUN
     if platform == 'LAPTOP':
         out_dir1 = '4_u-bg610_RA2M_CON/OUT_R1/'
-        out_dir2 = '7_u-bn068_RA2T_CON/OUT_R2_lam/'
+        out_dir2 = '7_u-bn068_RA2T_CON/OUT_R2_glm/'
         # out_dir3 = 'MET_DATA/'
-        out_dir4 = '7_u-bn068_RA2T_CON/OUT_R2_glm/'
+        out_dir4 = 'OUT_25H/'
     elif platform == 'JASMIN':
         out_dir1 = 'UM_RA2M/'
         out_dir2 = 'UM_CASIM-100/'
