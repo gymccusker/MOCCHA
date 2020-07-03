fix units

UM/threeRun_Comparison.py
@@ -1559,20 +1559,20 @@ def plot_CWC_RA2T(data1, data2, data3, month_flag, missing_files, out_dir1, out_
     plt.plot(np.nanmean(obs_data[obsname],0),np.nanmean(obs_data['height'],0), 'k--', linewidth = 3, label = 'Obs [g/m3]')
     ax4.fill_betweenx(np.nanmean(obs_data['height'],0),np.nanmean(obs_data[obsname],0) - np.nanstd(obs_data[obsname],0),
         np.nanmean(obs_data[obsname],0) + np.nanstd(obs_data[obsname],0), color = 'lightgrey', alpha = 0.5)
-    plt.plot(np.nanmean(data1[var],0),data1['height'], color = 'steelblue', linewidth = 3, label = label1)
-    ax4.fill_betweenx(data1['height'],np.nanmean(data1[var],0) - np.nanstd(data1[var],0),
-        np.nanmean(data1[var],0) + np.nanstd(data1[var],0), color = 'lightblue', alpha = 0.4)
-    plt.plot(np.nanmean(data2[var],0),data2['height'], color = 'purple', linewidth = 3, label = label2)
-    ax4.fill_betweenx(data2['height'],np.nanmean(data2[var],0) - np.nanstd(data2[var],0),
-        np.nanmean(data2[var],0) + np.nanstd(data2[var],0), color = 'thistle', alpha = 0.35)
+    plt.plot(np.nanmean(data1[var]*1e3,0),data1['height'], color = 'steelblue', linewidth = 3, label = label1)
+    ax4.fill_betweenx(data1['height'],np.nanmean(data1[var]*1e3,0) - np.nanstd(data1[var]*1e3,0),
+        np.nanmean(data1[var]*1e3,0) + np.nanstd(data1[var]*1e3,0), color = 'lightblue', alpha = 0.4)
+    plt.plot(np.nanmean(data2[var]*1e3,0),data2['height'], color = 'purple', linewidth = 3, label = label2)
+    ax4.fill_betweenx(data2['height'],np.nanmean(data2[var]*1e3,0) - np.nanstd(data2[var]*1e3,0),
+        np.nanmean(data2[var]*1e3,0) + np.nanstd(data2[var]*1e3,0), color = 'thistle', alpha = 0.35)
     if out_dir4 == 'OUT_25H/':
         plt.plot(np.nanmean(ifs_data[modname3],0),np.nanmean(ifs_data['height'],0), color = 'darkorange', linewidth = 3, label = 'ECMWF_IFS [g/m3]')
         ax4.fill_betweenx(np.nanmean(ifs_data['height'],0),np.nanmean(ifs_data[modname3],0) - np.nanstd(ifs_data[modname3],0),
             np.nanmean(ifs_data[modname3],0) + np.nanstd(ifs_data[modname3],0), color = 'navajowhite', alpha = 0.15)
     else:
-        plt.plot(np.nanmean(data3[var],0),data3['height'], color = 'forestgreen', linewidth = 3, label = label3)
-        ax4.fill_betweenx(data3['height'],np.nanmean(data3[var],0) - np.nanstd(data3[var],0),
-            np.nanmean(data3[var],0) + np.nanstd(data3[var],0), color = 'mediumaquamarine', alpha = 0.15)
+        plt.plot(np.nanmean(data3[var]*1e3,0),data3['height'], color = 'forestgreen', linewidth = 3, label = label3)
+        ax4.fill_betweenx(data3['height'],np.nanmean(data3[var]*1e3,0) - np.nanstd(data3[var]*1e3,0),
+            np.nanmean(data3[var]*1e3,0) + np.nanstd(data3[var]*1e3,0), color = 'mediumaquamarine', alpha = 0.15)
     plt.xlabel(lab)
     plt.ylabel('Height [m]')
     plt.ylim([0,10000])