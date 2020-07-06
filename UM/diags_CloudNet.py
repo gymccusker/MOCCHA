a3575168946bc231ea8c9ce9379312a18c5aa96f
gyoung410 <G.Young1@leeds.ac.uk>
2020-07-06 17:10:42 +0100

    update 14_ comment

--- UM/diags_CloudNet.py
+++ UM/diags_CloudNet.py
@@ -801,7 +801,7 @@ def main():
     ## 11_u-bq798_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Meyers
     ## 12_u-br210_RA1M_CASIM/           # UKCA daily averaged aerosol profiles, identical suite = u-bm507
     ## 13_u-br409_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; passive aerosol processing
-    ## 14_u-bu5_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit
+    ## 14_u-bu570_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit
     ## 15_u-bu687_RA2M_CON/           # Wilson and Ballard 1999 uphys; new RHcrit
 
     # -------------------------------------------------------------
