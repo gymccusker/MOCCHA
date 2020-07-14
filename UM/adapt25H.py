786f6c85017b10b238ac6cf8ac7233b9237375e8
gyoung410 <G.Young1@leeds.ac.uk>
2020-07-14 10:23:35 +0100

    set up for 7_u-bn068_RA2T_CON/OUT_24h/

--- UM/adapt25H.py
+++ UM/adapt25H.py
@@ -331,7 +331,7 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '14_u-bu570_RA1M_CASIM/OUT_24h/'
+    out_dir = '7_u-bn068_RA2T_CON/OUT_24h/'
     out_dir3 = 'MET_DATA/'
 
     ### TESTING/domain_tests/umnsaa_pa000
