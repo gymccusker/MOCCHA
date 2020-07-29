3882e33464a7c728e0ca7240d16ff88a2fc02cde
gyoung410 <G.Young1@leeds.ac.uk>
2020-07-29 09:14:01 +0100

    set up for 7_u-bn068_RA2T_CON/

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -7699,7 +7699,7 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '4_u-bg610_RA2M_CON/'
+    out_dir = '7_u-bn068_RA2T_CON/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 4_u-bg610_RA2M_CON/              # Wilson and Ballard 1999 uphys
