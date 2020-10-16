f1e6176684f72701f842ea73db0a7ea4f3269908
gyoung410 <G.Young1@leeds.ac.uk>
2020-10-16 12:13:25 +0100

    update filename for testing

--- UM/threeRun_Comparison.py
+++ UM/threeRun_Comparison.py
@@ -925,7 +925,7 @@ def plot_CASIM_NdropTimeseries(data1, data2, data3, month_flag, missing_files, o
     print ('')
 
     if month_flag == -1:
-        fileout = '../FIGS/CASIM/CASIM-100_CASIM-AeroProf_CRF-TS-Obs_Cv_Ndrop_Qliq_hourlyCRFobs.png'
+        fileout = '../FIGS/CASIM/CASIM-100_CASIM-AeroProf_CRF-TS-Obs_Cv_Ndrop_Qliq_hourlyCRFobs_newColours.png'
     plt.savefig(fileout)
     plt.show()
 
