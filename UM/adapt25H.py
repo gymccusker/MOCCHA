a3710b9f08f8f6193644fd4dbc0b4fb020c6e8d4
gyoung410 <G.Young1@leeds.ac.uk>
2020-12-03 12:06:33 +0000

    set up for new UM_RA2T LAM v3 dates

--- UM/adapt25H.py
+++ UM/adapt25H.py
@@ -555,14 +555,14 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '7_u-bn068_RA2T_CON/OUT_R2_RadPA/'
+    out_dir = '7_u-bn068_RA2T_CON/OUT_R3_24h/'
     out_dir3 = 'MET_DATA/'
 
     ### TESTING/domain_tests/umnsaa_pa000
     ### 4_u-bg610_RA2M_CON/OUT_R1/papbpc_combined/
     ### 5_u-bl661_RA1M_CASIM/OUT_R0_24H/       # 100/cc accum mode aerosol
     ### 6_u-bm410_RA1M_CASIM/OUT/       # 200/cc accum mode aerosol
-    ### 7_u-bn068_RA2T_CON/OUT_R2_lam_24h/              # RA2T_CON nest + global 4D stash
+    ### 7_u-bn068_RA2T_CON/OUT_R3_24h/              # RA2T_CON nest + global 4D stash
     ### 8_u-bp738_RA2M_CON/              # ERAI
     ### 10_u-bq791_RA1M_CASIM/OUT_24h/      # CASIM with 100/cc accum mode soluble aerosol w/Fletcher Nice param
     ### 11_u-bq798_RA1M_CASIM/OUT_24h/      # CASIM with 100/cc accum mode soluble aerosol w/Meyers Nice param
@@ -640,15 +640,16 @@ def main():
                 '20180910_oden_metum_a.nc','20180911_oden_metum_a.nc','20180912_oden_metum_a.nc','20180913_oden_metum_a.nc',
                 '20180914_oden_metum_a.nc']
     else:
-        moccha_names = ['20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
-                '20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
-                '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
-                '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
+        moccha_names = [#'20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
+                #'20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
+                # '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
+                # '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc',
+                '20180828_oden_metum.nc',
                 '20180829_oden_metum.nc','20180830_oden_metum.nc','20180831_oden_metum.nc','20180901_oden_metum.nc',
                 '20180902_oden_metum.nc','20180903_oden_metum.nc','20180904_oden_metum.nc','20180905_oden_metum.nc',
-                '20180906_oden_metum.nc','20180907_oden_metum.nc','20180908_oden_metum.nc','20180909_oden_metum.nc',
-                '20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc',
-                '20180914_oden_metum.nc']
+                '20180906_oden_metum.nc']#,'20180907_oden_metum.nc','20180908_oden_metum.nc','20180909_oden_metum.nc',
+                # '20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc',
+                # '20180914_oden_metum.nc']
 
     Aug_missing_files = ['20180812_oden_metum.nc','20180813_oden_metum.nc']
 
@@ -656,9 +657,9 @@ def main():
 
     moccha_missing_files = []
 
-    doy = np.arange(225,259)        ## set DOY for full moccha figures
+    # doy = np.arange(225,259)        ## set DOY for full moccha figures
     # doy = np.arange(243,259)        ## set DOY for subset of moccha figures
-    # doy = np.arange(240,248)        ## set DOY for subset of moccha figures
+    doy = np.arange(240,249)        ## set DOY for subset of moccha figures
     # doy = np.arange(248,258)        ## set DOY for subset of moccha figures
     # doy = np.arange(226,259)        ## set DOY for CASIM-AeroProf (17th Aug to 14th Sep)
 
