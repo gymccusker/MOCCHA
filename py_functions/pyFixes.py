aa49d4349221f380af280143b95dcd50cf849393
gyoung410 <G.Young1@leeds.ac.uk>
2020-12-14 12:02:25 +0000

    fixed! :D

--- py_functions/pyFixes.py
+++ py_functions/pyFixes.py
@@ -18,7 +18,7 @@ def py3_FixNPLoad(np):
     np_load_old = partial(np.load)
 
     # modify the default parameters of np.load
-    np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+    np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, encoding = 'latin1', **k)
 
     # call load_data with allow_pickle implicitly set to true
     # (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
