649e104e991aa31516db213db107aa0728dab773
gyoung410 <G.Young1@leeds.ac.uk>
2020-12-18 11:50:04 +0000

    update for 20_u-ca362_RA1M_CASIM/

--- UM/diags_CloudNet.py
+++ UM/diags_CloudNet.py
@@ -783,7 +783,7 @@ def main():
         ship_filename = '/nfs/a96/MOCCHA/working/gillian/ship/2018_shipposition_1hour.txt'
 
     ### CHOSEN RUN
-    out_dir = '17_u-bz429_RA1M_CASIM/'
+    out_dir = '20_u-ca362_RA1M_CASIM/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 4_u-bg610_RA2M_CON/              # Wilson and Ballard 1999 uphys
@@ -801,6 +801,7 @@ def main():
     ## 17_u-bz429_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; 15 min res 3D diagnostics
     ## 18_u-ca011_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; 15 min res 3D diagnostics; 1A BL Scheme
     ## 19_u-ca012_RA2T_CON/              # RA2T_CON nest + global 4D stash; includes diagnosed turbulent dissipation rate
+    ## 20_u-ca362_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; CICE sea ice albedo scheme
 
     # -------------------------------------------------------------
     # Extract from MASS with:
