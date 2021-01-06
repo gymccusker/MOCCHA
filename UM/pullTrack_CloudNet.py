7b9202d3521d4dea0e5ca8ea3e94092ca69914d6
gyoung410 <G.Young1@leeds.ac.uk>
2021-01-06 11:19:59 +0000

    works :) set up for 19_u-ca012_RA2T_CON/

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -7206,7 +7206,7 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '20_u-ca362_RA1M_CASIM/'
+    out_dir = '19_u-ca012_RA2T_CON/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 4_u-bg610_RA2M_CON/              # Wilson and Ballard 1999 uphys
@@ -7224,6 +7224,7 @@ def main():
     ## 17_u-bz429_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; 15 min res 3D diagnostics
     ## 18_u-ca011_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; 15 min res 3D diagnostics; 1A BL Scheme
     ## 19_u-ca012_RA2T_CON/              # RA2T_CON nest + global 4D stash; includes diagnosed turbulent dissipation rate
+    ## 20_u-ca362_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; CICE sea ice scheme
 
     #### run with nohup:
     ####    nohup python pullTrack_CloudNet.py > nohup_u-bn068_pullTrack_CloudNet.out &
