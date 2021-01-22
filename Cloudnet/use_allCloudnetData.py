e9f7cb742fc1d3c9abff7e191ec9f96fe6f1c6aa
gyoung410 <G.Young1@leeds.ac.uk>
2021-01-22 14:27:01 +0000

    reset for UM_CASIM-100

--- Cloudnet/use_allCloudnetData.py
+++ Cloudnet/use_allCloudnetData.py
@@ -6881,12 +6881,12 @@ def main():
     if platform == 'LAPTOP':
         ### model directories
         out_dir1 = '4_u-bg610_RA2M_CON/OUT_R1/'
-        out_dir2 = '12_u-br210_RA1M_CASIM/OUT_R0/'
+        out_dir2 = '14_u-bu570_RA1M_CASIM/OUT_R0/'
         out_dir3 = 'OUT_25H/'
         out_dir4 = '7_u-bn068_RA2T_CON/OUT_R2_lam/'
         ### cloudnet directories
         cloudnet_um1 = '4_u-bg610_RA2M_CON/'
-        cloudnet_um2 = '12_u-br210_RA1M_CASIM/'
+        cloudnet_um2 = '14_u-bu570_RA1M_CASIM/'
         cloudnet_um4 = '7_u-bn068_RA2T_CON/'
     elif platform == 'JASMIN':
         out_dir1 = 'UM_RA2M/'
