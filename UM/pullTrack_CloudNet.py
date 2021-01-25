a7eb9c134f4da8376e826297b54d0b1459a0118d
gyoung410 <G.Young1@leeds.ac.uk>
2021-01-25 11:52:19 +0000

    don't need to consider hgts=70 in pd stream of this run

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -6761,11 +6761,12 @@ def writePD_BL(cube, doutfile):
             dat.STASH = str(cube[d].attributes['STASH'])
             if not cube[d].standard_name == None: dat.standard_name = str(cube[d].standard_name)
             if not cube[d].long_name == None: dat.long_name = str(cube[d].long_name)
-            if np.size(cube[d].data,1) == 70:
-                dat[:,:] = cube[d].data
-            elif np.size(cube[d].data,1) == 69:
-                dat[:,:-1] = cube[d].data
-                dat[:,-1] = np.nan
+            # if np.size(cube[d].data,1) == 70:         ### need this for UM_RA2T_TKEdissrate
+            #     dat[:,:] = cube[d].data
+            # elif np.size(cube[d].data,1) == 69:
+            #     dat[:,:-1] = cube[d].data
+            #     dat[:,-1] = np.nan
+            dat[:,:] = cube[d].data
         elif np.ndim(cube[d]) == 1:
             dat = dataset.createVariable(cube[d].var_name, np.float64, ('forecast_time',), fill_value='-9999')
             dat.scale_factor = float(1)
