3eaa72bef0244e6e6694de51d7ddb46b02f78899
gyoung410 <G.Young1@leeds.ac.uk>
2021-03-11 11:51:41 +0000

    set up for 25_u-cc568_RA2M_CON/

--- UM/diags_CloudNet.py
+++ UM/diags_CloudNet.py
@@ -783,7 +783,7 @@ def main():
         ship_filename = '/nfs/a96/MOCCHA/working/gillian/ship/2018_shipposition_1hour.txt'
 
     ### CHOSEN RUN
-    out_dir = '24_u-cc324_RA2T_CON/'
+    out_dir = '25_u-cc568_RA2M_CON/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 4_u-bg610_RA2M_CON/              # Wilson and Ballard 1999 uphys
@@ -804,6 +804,7 @@ def main():
     ## 20_u-ca362_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; CICE sea ice albedo scheme
     ## 23_u-cc278_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; sea ice albedo options as GLM
     ## 24_u-cc324_RA2T_CON/             # RA2T_CON nest + global 4D stash. sea ice albedo (GLM+LAM) and extra BL diags (LAM) included
+    ## 25_u-cc568_RA2M_CON/             # Wilson and Ballard 1999 uphys. sea ice albedo and extra BL diags
 
     # -------------------------------------------------------------
     # Extract from MASS with:
