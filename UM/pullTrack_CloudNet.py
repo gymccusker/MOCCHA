5fbecb1d758e07394949e074099b199bd8d632ce
gyoung410 <G.Young1@leeds.ac.uk>
2021-04-15 11:53:16 +0100

    reset for 25_u-cc568_RA2M_CON/

--- UM/pullTrack_CloudNet.py
+++ UM/pullTrack_CloudNet.py
@@ -7229,7 +7229,7 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '24_u-cc324_RA2T_CON/'
+    out_dir = '25_u-cc568_RA2M_CON/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 4_u-bg610_RA2M_CON/              # Wilson and Ballard 1999 uphys
