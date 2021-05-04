5fc54e7e361eb72503347827594cd5c46a6b851b
gyoung410 <G.Young1@leeds.ac.uk>
2021-05-04 15:02:28 +0100

    use MT thresholding for WCs; exclude data <155m from models

--- Cloudnet/use_allCloudnetData.py
+++ Cloudnet/use_allCloudnetData.py
@@ -5814,34 +5814,137 @@ def plot_scaledBL_thetaE(data1, data2, data3, data4, um_data, ifs_data, misc_dat
         ifs_data['model_snow_Cv_filtered'][ifs_data['model_snow_Cv_filtered'] < 0.0] = np.nan
         misc_data['model_Cv_filtered'][misc_data['model_Cv_filtered'] < 0.0] = np.nan
         ra2t_data['model_Cv_filtered'][ra2t_data['model_Cv_filtered'] < 0.0] = np.nan
-    elif var == 'lwc_adiabatic':
-        obs_data['lwc_adiabatic'][obs_data['lwc_adiabatic'] == -999] = 0.0
-        # obs_data['lwc'][obs_data['lwc'] == 0] = np.nan
-        um_data['model_lwc'][um_data['model_lwc'] < 0.0] = 0.0
-        ifs_data['model_lwc'][ifs_data['model_lwc'] < 0.0] = 0.0
-        ifs_data['model_lwc'][ifs_data['model_lwc'] >= 20.0] = np.nan
-        misc_data['model_lwc'][misc_data['model_lwc'] < 0.0] = 0.0
-        ra2t_data['model_lwc'][ra2t_data['model_lwc'] < 0.0] = 0.0
-        #### change units to g/m3
-        obs_data['lwc_adiabatic'] = obs_data['lwc_adiabatic'] * 1e3
-        um_data['model_lwc'] = um_data['model_lwc'] * 1e3
-        misc_data['model_lwc'] = misc_data['model_lwc'] * 1e3
-        ra2t_data['model_lwc'] = ra2t_data['model_lwc'] * 1e3
-        ifs_data['model_lwc'] = ifs_data['model_lwc'] * 1e3
-    elif var == 'iwc':
-        obs_data['iwc'][obs_data['iwc'] == -999] = 0.0
-        # obs_data['iwc'][obs_data['iwc'] == 0] = np.nan
-        um_data['model_iwc_filtered'][um_data['model_iwc_filtered'] < 0.0] = 0.0
-        ifs_data['model_snow_iwc_filtered'][ifs_data['model_snow_iwc_filtered'] < 0.0] = 0.0
-        # ifs_data['model_snow_iwc_filtered'][ifs_data['model_snow_iwc_filtered'] >= 20.0] = np.nan
-        misc_data['model_iwc_filtered'][misc_data['model_iwc_filtered'] < 0.0] = 0.0
-        ra2t_data['model_iwc_filtered'][ra2t_data['model_iwc_filtered'] < 0.0] = 0.0
-        #### change units to g/m3
-        obs_data['iwc'] = obs_data['iwc'] * 1e3
-        um_data['model_iwc_filtered'] = um_data['model_iwc_filtered'] * 1e3
-        misc_data['model_iwc_filtered'] = misc_data['model_iwc_filtered'] * 1e3
-        ra2t_data['model_iwc_filtered'] = ra2t_data['model_iwc_filtered'] * 1e3
-        ifs_data['model_snow_iwc_filtered'] = ifs_data['model_snow_iwc_filtered'] * 1e3
+    # elif var == 'lwc_adiabatic':
+    #     obs_data['lwc_adiabatic'][obs_data['lwc_adiabatic'] == -999] = 0.0
+    #     # obs_data['lwc'][obs_data['lwc'] == 0] = np.nan
+    #     um_data['model_lwc'][um_data['model_lwc'] < 0.0] = 0.0
+    #     ifs_data['model_lwc'][ifs_data['model_lwc'] < 0.0] = 0.0
+    #     ifs_data['model_lwc'][ifs_data['model_lwc'] >= 20.0] = np.nan
+    #     misc_data['model_lwc'][misc_data['model_lwc'] < 0.0] = 0.0
+    #     ra2t_data['model_lwc'][ra2t_data['model_lwc'] < 0.0] = 0.0
+    #     #### change units to g/m3
+    #     obs_data['lwc_adiabatic'] = obs_data['lwc_adiabatic'] * 1e3
+    #     um_data['model_lwc'] = um_data['model_lwc'] * 1e3
+    #     misc_data['model_lwc'] = misc_data['model_lwc'] * 1e3
+    #     ra2t_data['model_lwc'] = ra2t_data['model_lwc'] * 1e3
+    #     ifs_data['model_lwc'] = ifs_data['model_lwc'] * 1e3
+    # elif var == 'iwc':
+    #     obs_data['iwc'][obs_data['iwc'] == -999] = 0.0
+    #     # obs_data['iwc'][obs_data['iwc'] == 0] = np.nan
+    #     um_data['model_iwc_filtered'][um_data['model_iwc_filtered'] < 0.0] = 0.0
+    #     ifs_data['model_snow_iwc_filtered'][ifs_data['model_snow_iwc_filtered'] < 0.0] = 0.0
+    #     # ifs_data['model_snow_iwc_filtered'][ifs_data['model_snow_iwc_filtered'] >= 20.0] = np.nan
+    #     misc_data['model_iwc_filtered'][misc_data['model_iwc_filtered'] < 0.0] = 0.0
+    #     ra2t_data['model_iwc_filtered'][ra2t_data['model_iwc_filtered'] < 0.0] = 0.0
+    #     #### change units to g/m3
+    #     obs_data['iwc'] = obs_data['iwc'] * 1e3
+    #     um_data['model_iwc_filtered'] = um_data['model_iwc_filtered'] * 1e3
+    #     misc_data['model_iwc_filtered'] = misc_data['model_iwc_filtered'] * 1e3
+    #     ra2t_data['model_iwc_filtered'] = ra2t_data['model_iwc_filtered'] * 1e3
+    #     ifs_data['model_snow_iwc_filtered'] = ifs_data['model_snow_iwc_filtered'] * 1e3
+
+    ### flag out large iwc/lwc values from IFS caused by missing dates
+    ifs_data['model_snow_iwc_filtered'][ifs_data['model_snow_iwc_filtered'] >= 5.0e-3] = np.nan
+    ifs_data['model_lwc'][ifs_data['model_lwc'] >= 0.4] = np.nan
+
+    ###----------------------------------------------------------------
+    ###         Calculate total water content
+    ###----------------------------------------------------------------
+
+    obs_data['twc'] = obs_data['lwc_adiabatic'] + obs_data['iwc']
+    # obs_data['twc'] = obs_data['lwc_adiabatic_inc_nolwp'] + obs_data['iwc']
+    um_data['model_twc'] = um_data['model_lwc'] + um_data['model_iwc_filtered']
+    misc_data['model_twc'] = misc_data['model_lwc'] + misc_data['model_iwc_filtered']
+    ifs_data['model_twc'] = ifs_data['model_lwc'] + ifs_data['model_snow_iwc_filtered']
+    ra2t_data['model_twc'] = ra2t_data['model_lwc'] + ra2t_data['model_iwc_filtered']
+
+    #### ---------------------------------------------------------------------------------------------------
+    #### ---------------------------------------------------------------------------------------------------
+    ####            CREATE TWC CLOUD MASK
+    #### ---------------------------------------------------------------------------------------------------
+    #### ---------------------------------------------------------------------------------------------------
+    ####-------------------------------------------------------------------------
+    ### from Michael's paper:
+    ####    "we consider a grid point cloudy when cloud water exceeded
+    ####    0.001 (0.0001) g kg-1 below 1 km (above 4 km), with linear
+    ####    interpolation in between."
+    ####-------------------------------------------------------------------------
+    twc_thresh_um = np.zeros([np.size(um_data['model_twc'],1)])
+    twc_thresh_ifs = np.zeros([np.size(ifs_data['model_twc'],1)])
+    ####-------------------------------------------------------------------------
+    ### first look at values below 1 km
+    ###     find Z indices <= 1km, then set twc_thresh values to 1e-6
+    um_lt1km = np.where(um_data['height'][0,:]<=1e3)
+    ifs_lt1km = np.where(ifs_data['height'][0,:]<=1e3)
+    twc_thresh_um[um_lt1km] = 1e-6
+    twc_thresh_ifs[ifs_lt1km] = 1e-6
+
+    ####-------------------------------------------------------------------------
+    ### next look at values above 4 km
+    ###     find Z indices >= 4km, then set twc_thresh values to 1e-7
+    um_lt1km = np.where(um_data['height'][0,:]>=4e3)
+    ifs_lt1km = np.where(ifs_data['height'][0,:]>=4e3)
+    twc_thresh_um[um_lt1km] = 1e-7
+    twc_thresh_ifs[ifs_lt1km] = 1e-7
+
+    ### find heights not yet assigned
+    um_intZs = np.where(twc_thresh_um == 0.0)
+    ifs_intZs = np.where(twc_thresh_ifs == 0.0)
+
+    ### interpolate for twc_thresh_um
+    x = [1e-6, 1e-7]
+    y = [1e3, 4e3]
+    f = interp1d(y, x)
+    twc_thresh_um[um_intZs] = f(um_data['height'][0,um_intZs].data)
+
+    ### interpolate for twc_thresh_ifs
+    twc_thresh_ifs[ifs_intZs] = f(ifs_data['height'][0,ifs_intZs].data)
+
+    ### plot profile of threshold as sanity check
+    # plt.plot(twc_thresh_um, um_data['height'][0,:])
+    # plt.plot(twc_thresh_ifs, ifs_data['height'][0,:]); plt.show()
+    # print (twc_thresh_um)
+
+    for t in range(0,np.size(um_data['model_twc'],0)):
+        for k in range(0,np.size(um_data['model_twc'],1)):
+            if obs_data['twc'][t,k] < twc_thresh_um[k]:
+                obs_data['twc'][t,k] = np.nan
+                obs_data['lwc_adiabatic'][t,k] = np.nan
+                obs_data['iwc'][t,k] = np.nan
+            if um_data['model_twc'][t,k] < twc_thresh_um[k]:
+                um_data['model_twc'][t,k] = np.nan
+                um_data['model_lwc'][t,k] = np.nan
+                um_data['model_iwc_filtered'][t,k] = np.nan
+            if misc_data['model_twc'][t,k] < twc_thresh_um[k]:
+                misc_data['model_twc'][t,k] = np.nan
+                misc_data['model_lwc'][t,k] = np.nan
+                misc_data['model_iwc_filtered'][t,k] = np.nan
+            if ra2t_data['model_twc'][t,k] < twc_thresh_um[k]:
+                ra2t_data['model_twc'][t,k] = np.nan
+                ra2t_data['model_lwc'][t,k] = np.nan
+                ra2t_data['model_iwc_filtered'][t,k] = np.nan
+        for k in range(0,np.size(ifs_data['model_twc'],1)):
+            if ifs_data['model_twc'][t,k] < twc_thresh_ifs[k]:
+                ifs_data['model_twc'][t,k] = np.nan
+                ifs_data['model_lwc'][t,k] = np.nan
+                ifs_data['model_snow_iwc_filtered'][t,k] = np.nan
+
+    # #### ---------------------------------------------------------------
+    # #### UNIT CONVERSION
+    # #### ---------------------------------------------------------------
+    #### change units to g/m3
+    obs_data['iwc'] = obs_data['iwc'] * 1e3
+    um_data['model_iwc_filtered'] = um_data['model_iwc_filtered'] * 1e3
+    misc_data['model_iwc_filtered'] = misc_data['model_iwc_filtered'] * 1e3
+    ra2t_data['model_iwc_filtered'] = ra2t_data['model_iwc_filtered'] * 1e3
+    ifs_data['model_snow_iwc_filtered'] = ifs_data['model_snow_iwc_filtered'] * 1e3
+
+    obs_data['lwc_adiabatic'] = obs_data['lwc_adiabatic'] * 1e3
+    um_data['model_lwc'] = um_data['model_lwc'] * 1e3
+    misc_data['model_lwc'] = misc_data['model_lwc'] * 1e3
+    ra2t_data['model_lwc'] = ra2t_data['model_lwc'] * 1e3
+    ifs_data['model_lwc'] = ifs_data['model_lwc'] * 1e3
+
 
     # #### ---------------------------------------------------------------
     # #### ONLY LOOK AT SONDES FROM THE DRIFT
@@ -6273,6 +6376,9 @@ def plot_scaledBL_thetaE(data1, data2, data3, data4, um_data, ifs_data, misc_dat
         ### for main inversion
         ###-----------------------------------------------------------------------------------------
         ### create array of height points under the identified inversion
+        ###         only include heights above 155m
+        um_Zindex = np.where(um_data['height'][i,:] < 155.0)
+        ifs_Zindex = np.where(ifs_data['height'][i,:] < 155.0)
         if data1['inversions']['invbase_kIndex'][i] >= 0.0:
             hgts1 = um_data['height'][i,:int(data1['inversions']['invbase_kIndex'][i])]
         else:
@@ -9440,8 +9546,8 @@ def main():
     # plot bivariate distributions
     #           first, set model data < 156 m to NaN for direct comparisons with observations
     # -------------------------------------------------------------
-    obs_data, um_data, misc_data, ifs_data, ra2t_data = fix_lowLevelData(obs_data, um_data, misc_data, ifs_data, ra2t_data, month_flag, missing_files, doy, varlist_obs, varlist_um, varlist_ifs)
-    figure = plot_BiVAR(um_data, ifs_data, misc_data, ra2t_data, obs_data, obs, month_flag, missing_files, cn_um_out_dir, doy, obs_switch, data1, data2, data3, data4, nanind, wcind, out_dir1)
+    # obs_data, um_data, misc_data, ifs_data, ra2t_data = fix_lowLevelData(obs_data, um_data, misc_data, ifs_data, ra2t_data, month_flag, missing_files, doy, varlist_obs, varlist_um, varlist_ifs)
+    # figure = plot_BiVAR(um_data, ifs_data, misc_data, ra2t_data, obs_data, obs, month_flag, missing_files, cn_um_out_dir, doy, obs_switch, data1, data2, data3, data4, nanind, wcind, out_dir1)
 
     # -------------------------------------------------------------
     # make obs comparison fig between um and ifs grids
@@ -9493,10 +9599,10 @@ def main():
     # if cn_ifs_out_dir[0] == 'cloud-fraction-ecmwf-grid/2018/': var = 'Cv'
     # if cn_ifs_out_dir[0] == 'lwc-scaled-ecmwf-grid/2018/': var = 'lwc'
     # if cn_ifs_out_dir[0] == 'iwc-Z-T-ecmwf-grid/2018/': var = 'iwc'
-    var = 'Cv'
+    var = 'iwc'
 
-    # obs_data, um_data, misc_data, ifs_data, ra2t_data = fix_lowLevelData(obs_data, um_data, misc_data, ifs_data, ra2t_data, month_flag, missing_files, doy, varlist_obs, varlist_um, varlist_ifs)
-    # figure = plot_scaledBL_thetaE(data1, data2, data3, data4, um_data, ifs_data, misc_data, ra2t_data, obs_data, month_flag, missing_files, out_dir1, out_dir2, out_dir3, obs, doy, label1, label2, label3, label4, var)
+    obs_data, um_data, misc_data, ifs_data, ra2t_data = fix_lowLevelData(obs_data, um_data, misc_data, ifs_data, ra2t_data, month_flag, missing_files, doy, varlist_obs, varlist_um, varlist_ifs)
+    figure = plot_scaledBL_thetaE(data1, data2, data3, data4, um_data, ifs_data, misc_data, ra2t_data, obs_data, month_flag, missing_files, out_dir1, out_dir2, out_dir3, obs, doy, label1, label2, label3, label4, var)
     # figure = plot_scaledBLCv_thetaE(data1, data2, data3, data4, um_data, ifs_data, misc_data, obs_data, month_flag, missing_files, out_dir1, out_dir2, out_dir3, obs, doy, label1, label2, label3)
     # figure = plot_scaledBLCv_JVInv(data1, data2, data3, um_data, ifs_data, misc_data, obs_data, month_flag, missing_files, out_dir1, out_dir2, out_dir3, obs, doy, label1, label2, label3)
     # figure = plot_scaledBLlwc(data1, data2, data3, um_data, ifs_data, misc_data, obs_data, month_flag, missing_files, out_dir1, out_dir2, out_dir3, obs, doy, label1, label2, label3)
