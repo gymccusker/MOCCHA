43a1517e88aeed9b3ef6030104e0b897e24e46a6
gyoung410 <G.Young1@leeds.ac.uk>
2021-05-20 13:26:51 +0100

    add if statements to bypass height2 assignment for GLM files

--- UM/adapt25H.py
+++ UM/adapt25H.py
@@ -155,9 +155,10 @@ def combineNC(nc1, nc2, filename1, filename2, out_dir):
     ## Data dimensions
     ###################################
     forecast_time = nc.createDimension('forecast_time', 25)
-    if out_dir[-6:-1] != 'RadPA':
+    if np.logical_or(out_dir[-6:-1] != 'RadPA':
         height = nc.createDimension('height', np.size(nc1.variables['height']))
-        height2 = nc.createDimension('height2', np.size(nc1.variables['height2']))
+        if np.logical_or(out_dir[-8:-5] != 'GLM':
+            height2 = nc.createDimension('height2', np.size(nc1.variables['height2']))
 
     ###################################
     ## Dimensions variables
@@ -173,6 +174,7 @@ def combineNC(nc1, nc2, filename1, filename2, out_dir):
     timem[24] = 24.0    ### hard code since nc2[0] = 0.0
     print ('time shape = ' + str(timem.shape))
 
+
     if out_dir[-6:-1] != 'RadPA':
         #### height
         height = nc.createVariable('height', np.float64, ('height',), fill_value='-9999')
@@ -183,14 +185,15 @@ def combineNC(nc1, nc2, filename1, filename2, out_dir):
         height.long_name = 'height'
         height[:] = nc1.variables['height'][:]      ### forecast time (ignore first 12h)
 
-        # #### height2
-        height2 = nc.createVariable('height2', np.float64, ('height2',), fill_value='-9999')
-        height2.scale_factor = float(1)
-        height2.add_offset = float(0)
-        height2.comment = ''
-        height2.units = 'm'
-        height2.long_name = 'height2'
-        height2[:] = nc1.variables['height2'][:]      ### forecast time (ignore first 12h)
+        if np.logical_or(out_dir[-8:-5] != 'GLM':
+            # #### height2
+            height2 = nc.createVariable('height2', np.float64, ('height2',), fill_value='-9999')
+            height2.scale_factor = float(1)
+            height2.add_offset = float(0)
+            height2.comment = ''
+            height2.units = 'm'
+            height2.long_name = 'height2'
+            height2[:] = nc1.variables['height2'][:]      ### forecast time (ignore first 12h)
 
     ###################################
     ## Create DIAGNOSTICS
@@ -775,15 +778,16 @@ def main():
                 '20180910_oden_metum_a.nc','20180911_oden_metum_a.nc','20180912_oden_metum_a.nc','20180913_oden_metum_a.nc',
                 '20180914_oden_metum_a.nc']
     else:
-        moccha_names = ['20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
-                '20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
-                '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
-                '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
-                '20180829_oden_metum.nc','20180830_oden_metum.nc','20180831_oden_metum.nc','20180901_oden_metum.nc',
-                '20180902_oden_metum.nc','20180903_oden_metum.nc','20180904_oden_metum.nc','20180905_oden_metum.nc',
-                '20180906_oden_metum.nc','20180907_oden_metum.nc','20180908_oden_metum.nc','20180909_oden_metum.nc',
-                '20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc',
-                '20180914_oden_metum.nc']
+        moccha_names = [#'20180814_oden_metum.nc','20180815_oden_metum.nc','20180816_oden_metum.nc',
+                # '20180817_oden_metum.nc','20180818_oden_metum.nc','20180819_oden_metum.nc','20180820_oden_metum.nc',
+                # '20180821_oden_metum.nc','20180822_oden_metum.nc','20180823_oden_metum.nc','20180824_oden_metum.nc',
+                # '20180825_oden_metum.nc','20180826_oden_metum.nc','20180827_oden_metum.nc','20180828_oden_metum.nc',
+                # '20180829_oden_metum.nc','20180830_oden_metum.nc',
+                '20180831_oden_metum.nc','20180901_oden_metum.nc',
+                '20180902_oden_metum.nc','20180903_oden_metum.nc','20180904_oden_metum.nc','20180905_oden_metum.nc']#,
+                # '20180906_oden_metum.nc','20180907_oden_metum.nc','20180908_oden_metum.nc','20180909_oden_metum.nc',
+                # '20180910_oden_metum.nc','20180911_oden_metum.nc','20180912_oden_metum.nc','20180913_oden_metum.nc',
+                # '20180914_oden_metum.nc']
 
     Aug_missing_files = ['20180812_oden_metum.nc','20180813_oden_metum.nc']
 
@@ -791,10 +795,10 @@ def main():
 
     moccha_missing_files = []
 
-    doy = np.arange(225,259)        ## set DOY for full moccha figures
+    # doy = np.arange(225,259)        ## set DOY for full moccha figures
     # doy = np.arange(245,253)        ## set DOY for subset of moccha figures
     # doy = np.arange(240,248)        ## set DOY for subset of moccha figures (28 Aug to 4 Sep)
-    # doy = np.arange(248,258)        ## set DOY for subset of moccha figures
+    doy = np.arange(243,249)        ## set DOY for subset of moccha figures
     # doy = np.arange(226,259)        ## set DOY for CASIM-AeroProf (17th Aug to 14th Sep)
 
     ## Choose month:
