424317c08f31f680a8808da63f6c8065da4ea261
gyoung410 <G.Young1@leeds.ac.uk>
2021-05-20 10:27:27 +0100

    reset for 28_u-ce627_RA2T_CON/OUT_R0_GLM_24h/

--- UM/adapt25H.py
+++ UM/adapt25H.py
@@ -679,7 +679,7 @@ def main():
         position_filename = 'AUX_DATA/POSITION_UNROTATED.csv'
 
     ### CHOSEN RUN
-    out_dir = '25_u-cc568_RA2M_CON/OUT_R1_24h/'
+    out_dir = '28_u-ce627_RA2T_CON/OUT_R0_GLM_24h/'
     out_dir3 = 'MET_DATA/'
 
     ### TESTING/domain_tests/umnsaa_pa000
@@ -703,6 +703,7 @@ def main():
     ## 24_u-cc324_RA2T_CON/OUT_R0_LAM_24h/             # RA2T_CON nest + global 4D stash. sea ice albedo (GLM+LAM) and extra BL diags (LAM) included
     ## 25_u-cc568_RA2M_CON/OUT_R1_24h/             # Wilson and Ballard 1999 uphys. sea ice albedo and extra BL diags
     ## 26_u-cd847_RA1M_CASIM/OUT_R0_24h/           # UKCA daily averaged aerosol profiles, identical suite = u-cd852. GA6 albedo options.
+    ## 28_u-ce627_RA2T_CON/OUT_R0_GLM_24h/            # RA2T_CON nest + global 4D stash. sea ice albedo (GLM+LAM) and extra BL diags (LAM) included. Mid-level convection switched off in GLM.
 
 
     print ('******')
