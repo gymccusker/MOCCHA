3f93c7761296dbcfeb49ffe7763f4344338b4288
gyoung410 <G.Young1@leeds.ac.uk>
2021-07-14 10:36:08 +0100

    update for 30_u-cg179_RA1M_CASIM/

--- UM/diags_CloudNet.py
+++ UM/diags_CloudNet.py
@@ -783,7 +783,7 @@ def main():
         ship_filename = '/nfs/a96/MOCCHA/working/gillian/ship/2018_shipposition_1hour.txt'
 
     ### CHOSEN RUN
-    out_dir = '28_u-ce627_RA2T_CON/'
+    out_dir = '30_u-cg179_RA1M_CASIM/'
     date_dir = os.listdir(root_dir + out_dir)
 
     ## 4_u-bg610_RA2M_CON/              # Wilson and Ballard 1999 uphys
@@ -808,6 +808,7 @@ def main():
     ## 26_u-cd847_RA1M_CASIM/           # UKCA daily averaged aerosol profiles, GA6 albedo options. identical suite = u-cd852
     ## 27_u-ce112_RA1M_CASIM/           # UKCA daily averaged aerosol profiles, GA6 albedo options. passive aerosol processing.
     ## 28_u-ce627_RA2T_CON/             # RA2T_CON nest + global 4D stash. sea ice albedo (GLM+LAM) and extra BL diags (LAM) included. Mid-level convection switched off in GLM.
+    ## 30_u-cg179_RA1M_CASIM/           # 100/cc accum mode aerosol; ARG + Cooper; new RHcrit; sea ice albedo options as GLM; passive aerosol processing
 
     # -------------------------------------------------------------
     # Extract from MASS with:
