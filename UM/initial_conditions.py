cd247ce1691e5d3c8fe9138eb4890c339a694a8a
gymccusker <G.Young1@leeds.ac.uk>
2022-02-11 10:39:22 +0000

    bloody typo

--- UM/initial_conditions.py
+++ UM/initial_conditions.py
@@ -1122,7 +1122,7 @@ def main():
         ship_filename = '/nfs/a96/MOCCHA/working/gillian/ship/2018_shipposition_1hour.txt'
 
     ### CHOSEN RUN
-    out_dir = '24_u-cc324_RA2T_CON//'
+    out_dir = '24_u-cc324_RA2T_CON/'
     out_dir_glm = '24_u-cc324_RA2T_CON/'
     date_dir = os.listdir(root_dir + out_dir)
 
