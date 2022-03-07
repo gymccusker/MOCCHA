9bad15094ea41dda3267a832620f5811727a9f9c
gymccusker <G.Young1@leeds.ac.uk>
2022-03-07 12:31:57 +0000

    find 0th rather than <=1?

--- UM/initial_conditions.py
+++ UM/initial_conditions.py
@@ -114,7 +114,7 @@ def trackShip(data, date):
     print (data)
 
     trackShip_start = np.where(np.logical_and(np.logical_and(data.values[:,2]==day,data.values[:,1]==month),data.values[:,3]>=12))
-    trackShip_end = np.where(np.logical_and(np.logical_and(data.values[:,2]==endpt,data.values[:,1]==month),data.values[:,3]<=1))
+    trackShip_end = np.where(np.logical_and(np.logical_and(data.values[:,2]==endpt,data.values[:,1]==month),data.values[:,3]==0))
 
     print (trackShip_start[0])
     print (trackShip_end[0])
