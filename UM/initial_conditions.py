c8cf9c40b243f90c318bf8cad1e33d422a55117c
gymccusker <G.Young1@leeds.ac.uk>
2022-03-07 12:20:56 +0000

    still not happy, add prints

--- UM/initial_conditions.py
+++ UM/initial_conditions.py
@@ -101,9 +101,17 @@ def trackShip(data, date):
     ## DEFINE METUM PERIOD (CLOUDNET COMPARISON)
     ###################################
 
+    print ('Extracting: ' + date)
+
     month = int(date[5])
     day = int(date[6:8])
     endpt = str(int(day)+2)
+
+    print ('month = ' + str(month))
+    print ('start point = ' + str(day))
+    print ('end point = ' + str(endpt))
+
+    print (data)
 
     trackShip_start = np.where(np.logical_and(np.logical_and(data.values[:,2]==day,data.values[:,1]==month),data.values[:,3]>=12))
     trackShip_end = np.where(np.logical_and(np.logical_and(data.values[:,2]==endpt,data.values[:,1]==month),data.values[:,3]<=1))
