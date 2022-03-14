15877eccbac2480b904c833878ab7d9bc083bd95
gymccusker <G.Young1@leeds.ac.uk>
2022-03-14 14:11:34 +0000

    index date and 0

--- UM/initial_conditions.py
+++ UM/initial_conditions.py
@@ -261,7 +261,7 @@ def plot_cartmap(ship_data, cube, date_dir):
 
         ### Plot tracks as line plot
         for i in range(0, len(ilon)-1):
-            iplt.scatter(cube[diag].dim_coords[2][int(ilon[i] + xoffset)], cube[diag].dim_coords[1][int(ilat[i] + yoffset)],color='black')
+            iplt.scatter(cube[date][0].dim_coords[2][int(ilon[i] + xoffset)], cube[date][0].dim_coords[1][int(ilat[i] + yoffset)],color='black')
 
 
         plt.legend()
