134d63041eb959bf5653df0e6f033e78fd1a4546
gymccusker <G.Young1@leeds.ac.uk>
2022-03-15 15:12:48 +0000

    need varnames

--- UM/initial_conditions.py
+++ UM/initial_conditions.py
@@ -11,6 +11,7 @@ import numpy as np
 from netCDF4 import Dataset
 import numpy as np
 import diags_MOCCHA as diags
+import diags_varnames as varnames
 import cartopy.crs as ccrs
 import iris
 import matplotlib.pyplot as plt
