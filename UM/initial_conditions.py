9cc83aa2a18198546d61dce6dea7aae92f1aa5a8
gymccusker <G.Young1@leeds.ac.uk>
2022-04-06 12:25:52 +0100

    testing...

--- UM/initial_conditions.py
+++ UM/initial_conditions.py
@@ -364,7 +364,7 @@ def plot_cartmap(ship_data, cube, date_dir, model, case_study):
             ## plot UM nest
             #################################################################
             ### draw outline of grid
-            if model == 'lam': qplt.outline(cube[date][0][0,50:,:])
+            if model == 'lam': qplt.outline(cube[date][0][0,70:,10:80])
 
             if case_study == True:
 
