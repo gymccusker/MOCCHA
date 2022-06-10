5ae4648ba9dacea62a254313f1fccebfcce39892
gymccusker <G.Young1@leeds.ac.uk>
2022-06-10 12:55:42 +0100

    plot against rho dimcoords

--- UM/initial_conditions.py
+++ UM/initial_conditions.py
@@ -3671,7 +3671,7 @@ def main():
     plt.subplot(121)
     plt.plot(data['temperature'],startdump[0].dim_coords[0].points)
     plt.subplot(122)
-    plt.plot(rho,startdump[0].dim_coords[0].points)
+    plt.plot(rho,startdump[2].dim_coords[0].points)
     plt.show()
 
 
